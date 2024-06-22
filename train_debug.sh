7393eb5990ed1455ec7d119fa3e26a78cfd02e2b
lwz <905328134@qq.com>
2024-06-23 00:30:12 +0800

    update to lightning 2.0

--- train_debug.sh
+++ train_debug.sh
@@ -1,16 +1,21 @@
 python image_retrieval/cli.py fit \
-    --data.dataset_name flickr8k \
-    --data.artifact_id wandb/clip.lightning-image_retrieval/flickr-8k:latest \
-    --model.image_encoder_alias resnet18 \
+    --data.dataset_name flickr30k \
+    --data.artifact_id wandb/clip.lightning-image_retrieval/flickr-30k:latest \
+    --data.train_batch_size 128 \
+    --data.val_batch_size 128 \
+    --model.image_encoder_alias resnet50 \
     --model.text_encoder_alias distilbert-base-uncased \
-    --model.image_embedding_dims 512 \
-    --model.image_encoder_lr 0.0001 \
+    --model.image_embedding_dims 2048 \
+    --model.image_encoder_lr 8e-4 \
     --model.image_encoder_pretrained true \
     --model.image_encoder_trainable true \
+    --model.text_embedding_dims 768 \
+    --model.text_encoder_lr 8e-5 \
+    --model.text_encoder_trainable true \
     --model.projection_dims 512 \
-    --model.image_encoder_lr 0.0001 \
+    --model.head_lr 8e-3 \
     --trainer.accelerator gpu \
-    --trainer.max_epochs 20 \
+    --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger \
     --trainer.logger.project CLIP \
