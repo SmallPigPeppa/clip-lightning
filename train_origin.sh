fef6bf318dae2ded760335b4803a86531c73e97f
lwz <905328134@qq.com>
2024-06-22 21:09:31 +0800

    update to lightning 2.0

--- train_origin.sh
+++ train_origin.sh
@@ -5,12 +5,12 @@ python image_retrieval/cli.py fit \
     --data.val_batch_size 128 \
     --data.max_length 200 \
     --model.image_encoder_alias resnet50 \
-    --model.text_encoder_alias distilbert-base-uncased \ 
+    --model.text_encoder_alias distilbert-base-uncased \
     --model.image_embedding_dims 2048 \
     --model.text_embedding_dims 768 \
     --model.projection_dims 256 \
-    --trainer.precision 16 \ 
-    --trainer.accelerator gpu \ 
+    --trainer.precision 16 \
+    --trainer.accelerator gpu \
     --trainer.max_epochs 20 \
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger
