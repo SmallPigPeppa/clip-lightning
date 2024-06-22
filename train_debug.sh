30a13bb99eb50033893d9cec65c7fa69f17ebce4
lwz <905328134@qq.com>
2024-06-22 22:56:27 +0800

    update to lightning 2.0

--- train_debug.sh
+++ train_debug.sh
@@ -1,10 +1,14 @@
 python image_retrieval/cli.py fit \
     --data.dataset_name flickr8k \
     --data.artifact_id wandb/clip.lightning-image_retrieval/flickr-8k:latest \
-    --data.train_batch_size 128 \
-    --data.val_batch_size 128 \
-    --model.image_encoder_alias resnet50 \
+    --model.image_encoder_alias resnet18 \
     --model.text_encoder_alias distilbert-base-uncased \
+    --model.image_embedding_dims 512 \
+    --model.image_encoder_lr 0.0001 \
+    --model.image_encoder_pretrained true \
+    --model.image_encoder_trainable true \
+    --model.projection_dims 512 \
+    --model.image_encoder_lr 0.0001 \
     --trainer.accelerator gpu \
     --trainer.max_epochs 20 \
     --trainer.log_every_n_steps 1 \
