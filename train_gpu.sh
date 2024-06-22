75c89d674bada18d66da49a7fdc4395adc509bf1
lwz <905328134@qq.com>
2024-06-22 21:35:38 +0800

    update to lightning 2.0

--- train_gpu.sh
+++ train_gpu.sh
@@ -1,8 +1,6 @@
 python image_retrieval/cli.py fit \
-    --data.dataset_name flickr8k \
-    --data.artifact_id wandb/clip.lightning-image_retrieval/flickr-8k:latest \
-    --data.train_batch_size 128 \
-    --data.val_batch_size 128 \
+    --data.dataset_name flickr30k \
+    --data.artifact_id wandb/clip.lightning-image_retrieval/flickr-30k:latest \
     --data.max_length 200 \
     --model.image_encoder_alias resnet50 \
     --model.text_encoder_alias distilbert-base-uncased \
@@ -13,4 +11,6 @@ python image_retrieval/cli.py fit \
     --trainer.accelerator gpu \
     --trainer.max_epochs 20 \
     --trainer.log_every_n_steps 1 \
-    --trainer.logger WandbLogger
+    --trainer.logger WandbLogger \
+    --trainer.logger.project CLIP \
+    --trainer.logger.log_model all
