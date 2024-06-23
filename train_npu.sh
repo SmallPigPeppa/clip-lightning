17dc2b62c783b35d7b83eaee3249ab3612611c22
lwz <905328134@qq.com>
2024-06-23 11:09:25 +0800

    update to lightning 2.0

--- train_npu.sh
+++ train_npu.sh
@@ -1,19 +1,19 @@
 python image_retrieval/cli.py fit \
     --data.dataset_name flickr30k \
     --data.artifact_id wandb/clip.lightning-image_retrieval/flickr-30k:latest \
-    --data.train_batch_size 128 \
-    --data.val_batch_size 128 \
+    --data.train_batch_size 16 \
+    --data.val_batch_size 16 \
     --model.image_encoder_alias resnet50 \
     --model.text_encoder_alias distilbert-base-uncased \
     --model.image_embedding_dims 2048 \
-    --model.image_encoder_lr 8e-4 \
+    --model.image_encoder_lr 1e-4 \
     --model.image_encoder_pretrained true \
     --model.image_encoder_trainable true \
     --model.text_embedding_dims 768 \
-    --model.text_encoder_lr 8e-5 \
+    --model.text_encoder_lr 1e-5 \
     --model.text_encoder_trainable true \
     --model.projection_dims 512 \
-    --model.head_lr 8e-3 \
+    --model.head_lr 1e-3 \
     --trainer.accelerator npu \
     --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
