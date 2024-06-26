4363d5680d19c2a4da9f5f6f42786aa672639b83
lwz <905328134@qq.com>
2024-06-26 17:28:06 +0800

    update to lightning 2.0

---
+++ image_retrieval/cli_aug.py
@@ -0,0 +1,29 @@
+from lightning.pytorch import cli
+from dataloaders.data_module_aug import ImageRetrievalDataModule
+from models.clip_model import CLIPDualEncoderModel
+from callbacks import LogPredictionCallback
+from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from lightning.pytorch.callbacks.lr_monitor import LearningRateMonitor
+
+import os
+os.environ['CURL_CA_BUNDLE'] = ''
+
+
+
+
+
+class CLI(cli.LightningCLI):
+    def add_arguments_to_parser(self, parser: cli.LightningArgumentParser) -> None:
+        parser.link_arguments("model.text_encoder_alias", "data.tokenizer_alias")
+        parser.add_lightning_class_args(
+            LogPredictionCallback, "log_prediction_callback"
+        )
+        parser.link_arguments(
+            "model.text_encoder_alias", "log_prediction_callback.tokenizer"
+        )
+        parser.add_lightning_class_args(ModelCheckpoint, "model_checkpoint")
+        parser.add_lightning_class_args(LearningRateMonitor, "lr_monitor")
+
+
+if __name__ == "__main__":
+    CLI(CLIPDualEncoderModel, ImageRetrievalDataModule, save_config_callback=None)
