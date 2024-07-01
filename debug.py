4dc6700f8e40f253facdfe21c5c7cbfd6d95b3dc
lwz <905328134@qq.com>
2024-07-01 23:16:02 +0800

    update to lightning 2.0

--- debug.py
+++ debug.py
@@ -15,19 +15,19 @@ if __name__ == "__main__":
     print(next(iter(a)))
     b = dm.val_dataloader()
     c = b.dataset
-    # print(b.dataset.dataset.images)
+    print(b.dataset.dataset.images)
 
 
-    artifact_id = ''
-    dataset_name = 'flickr30k'
-    config = './config.yaml'
-    tokenizer_alias = 'distilbert-base-uncased'
-    dm2 = ImageRetrievalDataModule(artifact_id=artifact_id,
-                                  dataset_name=dataset_name,
-                                  tokenizer_alias=tokenizer_alias)
-    dm2.setup()
-    a2 = dm2.train_dataloader()
-    print(next(iter(a2)))
-    b2 = dm2.val_dataloader()
-    c2 = b2.dataset
-    print(b2.dataset.dataset.images)
+    # artifact_id = ''
+    # dataset_name = 'flickr30k'
+    # config = './config.yaml'
+    # tokenizer_alias = 'distilbert-base-uncased'
+    # dm2 = ImageRetrievalDataModule(artifact_id=artifact_id,
+    #                               dataset_name=dataset_name,
+    #                               tokenizer_alias=tokenizer_alias)
+    # dm2.setup()
+    # a2 = dm2.train_dataloader()
+    # print(next(iter(a2)))
+    # b2 = dm2.val_dataloader()
+    # c2 = b2.dataset
+    # print(b2.dataset.dataset.images)
