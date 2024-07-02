6de1c03b8ba899acda02cc611521b2d18d4f1225
lwz <905328134@qq.com>
2024-07-02 18:11:59 +0800

    update to lightning 2.0

---
+++ debug2.py
@@ -0,0 +1,29 @@
+import timm
+
+model_name = 'resnet50'
+pretrained = True
+model = timm.create_model(
+    model_name, pretrained=pretrained, num_classes=0, global_pool="avg"
+)
+print(model)
+
+model2 = timm.create_model(
+    model_name, pretrained=pretrained
+)
+print(model2)
+
+model_name = 'vit_base_patch16_224.augreg2_in21k_ft_in1k'
+pretrained = True
+model3 = timm.create_model(
+    model_name, pretrained=pretrained
+)
+print(model3)
+
+
+
+model_name = 'vit_base_patch16_224.augreg2_in21k_ft_in1k'
+pretrained = True
+model4 = timm.create_model(
+    model_name, pretrained=pretrained,num_classes=0, global_pool="avg"
+)
+print(model4)
