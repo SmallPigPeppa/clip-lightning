39a069d2f515bf96ada3f7441f0e78cbcf8e594a
lwz <905328134@qq.com>
2024-07-07 14:40:01 +0800

    update to lightning 2.0

--- image_retrieval/models/clip_model.py
+++ image_retrieval/models/clip_model.py
@@ -56,69 +56,69 @@ class CLIPDualEncoderModel(LightningModule):
         self.text_encoder_lr = text_encoder_lr
         self.lr_warmup_epochs = lr_warmup_epochs
         self.save_hyperparameters()
-        self.log(self.hparams)
+        self.log("hparams", self.hparams)
 
-    def _compute_losses(self, image_embeddings, text_embeddings):
-        logits = (text_embeddings @ image_embeddings.T) / self.temperature
-        images_similarity = image_embeddings @ image_embeddings.T
-        texts_similarity = text_embeddings @ text_embeddings.T
-        targets = F.softmax(
-            (images_similarity + texts_similarity) / 2 * self.temperature, dim=-1
-        )
-        images_loss = (-targets.T * self.log_softmax(logits.T)).sum(1)
-        texts_loss = (-targets * self.log_softmax(logits)).sum(1)
-        return (images_loss + texts_loss) / 2.0
+        def _compute_losses(self, image_embeddings, text_embeddings):
+            logits = (text_embeddings @ image_embeddings.T) / self.temperature
+            images_similarity = image_embeddings @ image_embeddings.T
+            texts_similarity = text_embeddings @ text_embeddings.T
+            targets = F.softmax(
+                (images_similarity + texts_similarity) / 2 * self.temperature, dim=-1
+            )
+            images_loss = (-targets.T * self.log_softmax(logits.T)).sum(1)
+            texts_loss = (-targets * self.log_softmax(logits)).sum(1)
+            return (images_loss + texts_loss) / 2.0
 
-    def forward(self, inputs):
-        image_features = self.image_encoder(inputs["image"])
-        text_features = self.text_encoder(
-            input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
-        )
+        def forward(self, inputs):
+            image_features = self.image_encoder(inputs["image"])
+            text_features = self.text_encoder(
+                input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+            )
 
-        image_embeddings = self.image_projection(image_features)
-        text_embeddings = self.text_projection(text_features)
+            image_embeddings = self.image_projection(image_features)
+            text_embeddings = self.text_projection(text_features)
 
-        return image_embeddings, text_embeddings
+            return image_embeddings, text_embeddings
 
-    def configure_optimizers(self):
-        parameters = [
-            {"params": self.image_encoder.parameters(), "lr": self.image_encoder_lr},
-            {"params": self.text_encoder.parameters(), "lr": self.text_encoder_lr},
-            {
-                "params": itertools.chain(
-                    self.image_projection.parameters(),
-                    self.text_projection.parameters(),
-                ),
-                "lr": self.head_lr,
-                "weight_decay": self.weight_decay,
-            },
-        ]
-        optimizer = optim.Adam(parameters, weight_decay=self.weight_decay)
-        # optimizer = optim.SGD(parameters, weight_decay=5e-4, momentum=0.9)
-        base_lr = min(self.image_encoder_lr, self.text_encoder_lr, self.head_lr)
-        lr_scheduler = LinearWarmupCosineAnnealingLR(
-            optimizer,
-            warmup_epochs=self.lr_warmup_epochs,
-            max_epochs=self.trainer.max_epochs,
-            warmup_start_lr=0.01 * base_lr,
-            eta_min=0.01 * base_lr
-        )
+        def configure_optimizers(self):
+            parameters = [
+                {"params": self.image_encoder.parameters(), "lr": self.image_encoder_lr},
+                {"params": self.text_encoder.parameters(), "lr": self.text_encoder_lr},
+                {
+                    "params": itertools.chain(
+                        self.image_projection.parameters(),
+                        self.text_projection.parameters(),
+                    ),
+                    "lr": self.head_lr,
+                    "weight_decay": self.weight_decay,
+                },
+            ]
+            optimizer = optim.Adam(parameters, weight_decay=self.weight_decay)
+            # optimizer = optim.SGD(parameters, weight_decay=5e-4, momentum=0.9)
+            base_lr = min(self.image_encoder_lr, self.text_encoder_lr, self.head_lr)
+            lr_scheduler = LinearWarmupCosineAnnealingLR(
+                optimizer,
+                warmup_epochs=self.lr_warmup_epochs,
+                max_epochs=self.trainer.max_epochs,
+                warmup_start_lr=0.01 * base_lr,
+                eta_min=0.01 * base_lr
+            )
 
-        return {
-            "optimizer": optimizer,
-            "lr_scheduler": lr_scheduler,
-        }
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": lr_scheduler,
+            }
 
-    def training_step(self, batch, *args, **kwargs):
-        image_embeddings, text_embeddings = self.forward(batch)
-        loss = self._compute_losses(image_embeddings, text_embeddings).mean()
-        train_loss = self.all_gather(loss)
-        self.log("train/loss", train_loss.mean())
-        return loss
+        def training_step(self, batch, *args, **kwargs):
+            image_embeddings, text_embeddings = self.forward(batch)
+            loss = self._compute_losses(image_embeddings, text_embeddings).mean()
+            train_loss = self.all_gather(loss)
+            self.log("train/loss", train_loss.mean())
+            return loss
 
-    def validation_step(self, batch, *args, **kwargs):
-        image_embeddings, text_embeddings = self.forward(batch)
-        loss = self._compute_losses(image_embeddings, text_embeddings).mean()
-        val_loss = self.all_gather(loss)
-        self.log("val/loss", val_loss.mean())
-        return loss
+        def validation_step(self, batch, *args, **kwargs):
+            image_embeddings, text_embeddings = self.forward(batch)
+            loss = self._compute_losses(image_embeddings, text_embeddings).mean()
+            val_loss = self.all_gather(loss)
+            self.log("val/loss", val_loss.mean())
+            return loss
