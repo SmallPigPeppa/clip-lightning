c123701cf58ce915ce4c6148f2e4f4812de6a99e
lwz <905328134@qq.com>
2024-07-09 17:15:22 +0800

    update to lightning 2.0

--- image_retrieval/models/clip_model.py
+++ image_retrieval/models/clip_model.py
@@ -55,7 +55,7 @@ class CLIPDualEncoderModel(LightningModule):
         self.image_encoder_lr = image_encoder_lr
         self.text_encoder_lr = text_encoder_lr
         self.lr_warmup_epochs = lr_warmup_epochs
-        self.save_hyperparameters(logger=True)
+        self.save_hyperparameters()
 
     def _compute_losses(self, image_embeddings, text_embeddings):
         logits = (text_embeddings @ image_embeddings.T) / self.temperature
