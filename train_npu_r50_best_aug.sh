af5da5449f939ea3841666e58ed05018ab93b20c
lwz <905328134@qq.com>
2024-07-09 19:09:55 +0800

    update to lightning 2.0

--- train_npu_r50_best_aug.sh
+++ train_npu_r50_best_aug.sh
@@ -18,7 +18,7 @@ python image_retrieval/cli_aug.py fit \
     --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger \
-    --trainer.logger.project CLIP \
+    --trainer.logger.project CLIP-lightning \
     --trainer.logger.log_model False \
     --trainer.logger.name r50_best_aug \
     --lr_monitor.logging_interval epoch \
