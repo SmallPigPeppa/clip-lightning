86eb7e6f86816248b6460d22c28ca36038602264
lwz <905328134@qq.com>
2024-07-10 19:49:52 +0800

    update to lightning 2.0

--- train_npu_r50_aug_bs256.sh
+++ train_npu_r50_aug_bs256.sh
@@ -3,8 +3,8 @@
 python image_retrieval/cli_aug.py fit \
     --data.dataset_name flickr30k_aug \
     --data.artifact_id wandb/clip.lightning-image_retrieval/flickr-30k:latest \
-    --data.train_batch_size 128 \
-    --data.val_batch_size 128 \
+    --data.train_batch_size 256 \
+    --data.val_batch_size 256 \
     --data.config config.yaml \
     --model.image_encoder_alias resnet50 \
     --model.text_encoder_alias distilbert-base-uncased \
