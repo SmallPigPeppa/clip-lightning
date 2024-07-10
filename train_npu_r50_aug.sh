d86e39ba1b9ea44da9a6e8f6c7fbbe2278d66e1c
lwz <905328134@qq.com>
2024-07-10 15:33:49 +0800

    update to lightning 2.0

--- train_npu_r50_aug.sh
+++ train_npu_r50_aug.sh
@@ -9,14 +9,14 @@ python image_retrieval/cli_aug.py fit \
     --model.image_encoder_alias resnet50 \
     --model.text_encoder_alias distilbert-base-uncased \
     --model.image_encoder_pretrained True \
-    --model.image_embedding_dims 2048 \
-    --model.image_encoder_lr 1e-3 \
     --model.image_encoder_trainable True \
-    --model.text_embedding_dims 768 \
-    --model.text_encoder_lr 1e-3 \
+    --model.image_embedding_dims 2048 \
+    --model.image_encoder_lr 1e-4 \
     --model.text_encoder_trainable True \
+    --model.text_embedding_dims 768 \
+    --model.text_encoder_lr 1e-5 \
     --model.projection_dims 512 \
-    --model.head_lr 8e-3 \
+    --model.head_lr 1e-3 \
     --trainer.accelerator npu \
     --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
