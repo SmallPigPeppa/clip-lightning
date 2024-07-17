58202fc0b7b3862650c4b36514bf079a485f2070
lwz <905328134@qq.com>
2024-07-17 23:47:14 +0800

    update to lightning 2.0

--- image_retrieval/models/clip_model.py
+++ image_retrieval/models/clip_model.py
@@ -153,8 +153,8 @@ class CLIPDualEncoderModel(LightningModule):
         for name, logit in logits.items():
             ranking = torch.argsort(logit, descending=True).to(self.device)
             preds = torch.where(ranking == ground_truth)[1]
-            # metrics[f"{name}_mean_rank"] = preds.mean() + 1
-            # metrics[f"{name}_median_rank"] = np.floor(np.median(preds)) + 1
+            # metrics[f"{name}_mean_rank"] = preds.float().mean() + 1
+            # metrics[f"{name}_median_rank"] = preds.float().median() + 1
             # for k in [1, 5, 10]:
             for k in [1]:
                 metrics[f"{name}_R@{k}"] = (preds < k).float().mean() * 100  # Convert recall to percentage
@@ -173,9 +173,10 @@ class CLIPDualEncoderModel(LightningModule):
             ranking = torch.argsort(logit, descending=True)
             preds = torch.where(ranking == ground_truth)[1]
             preds = preds.detach().cpu().numpy()
-            metrics[f"{name}_mean_rank"] = preds.mean() + 1
-            metrics[f"{name}_median_rank"] = np.floor(np.median(preds)) + 1
-            for k in [1, 5, 10]:
+            # metrics[f"{name}_mean_rank"] = preds.mean() + 1
+            # metrics[f"{name}_median_rank"] = np.floor(np.median(preds)) + 1
+            # for k in [1, 5, 10]:
+            for k in [1]:
                 metrics[f"{name}_R@{k}"] = np.mean(preds < k) * 100  # Convert recall to percentage
 
         return metrics
