dfbdd6fcad3caf7ee056a5b568c4d635c576e734
lwz <905328134@qq.com>
2024-07-18 01:54:37 +0800

    update to lightning 2.0

--- train_npu_r50_aug_bs512_debug_ILR_PLR.sh
+++ train_npu_r50_aug_bs512_debug_ILR_PLR.sh
@@ -15,7 +15,7 @@ python image_retrieval/cli_aug.py fit \
     --model.head_lr 2e-3 \
     --model.text_encoder_trainable True \
     --model.text_embedding_dims 768 \
-    --model.text_encoder_lr 2e-5 \
+    --model.text_encoder_lr 1e-5 \
     --model.projection_dims 512 \
     --trainer.accelerator npu \
     --trainer.precision 16 \
