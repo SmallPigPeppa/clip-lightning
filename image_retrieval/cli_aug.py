21c61df06b09cb92bc1b4d11efcbc526f2478811
lwz <905328134@qq.com>
2024-07-17 21:03:25 +0800

    update to lightning 2.0

--- image_retrieval/cli_aug.py
+++ image_retrieval/cli_aug.py
@@ -6,20 +6,18 @@ from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from lightning.pytorch.callbacks.lr_monitor import LearningRateMonitor
 
 import os
+
 os.environ['CURL_CA_BUNDLE'] = ''
-
-
-
 
 
 class CLI(cli.LightningCLI):
     def add_arguments_to_parser(self, parser: cli.LightningArgumentParser) -> None:
         parser.link_arguments("model.text_encoder_alias", "data.tokenizer_alias")
         parser.link_arguments(
-            "model.train_batch_size", "data.train_batch_size"
+            "data.train_batch_size", "model.train_batch_size"
         )
         parser.link_arguments(
-            "model.val_batch_size", "data.val_batch_size"
+            "data.val_batch_size", "model.val_batch_size"
         )
 
         # log prediction
