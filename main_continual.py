86b391e9f0283aa1b77e8dc35f381853f15a897c
lwz <905328134@qq.com>
2024-07-18 01:26:40 +0800

    update to lightning 2.0

--- main_continual.py
+++ main_continual.py
@@ -44,6 +44,7 @@ def modify_command_for_task(base_command, params_dict, task_idx, num_tasks):
     for key, value in params_dict.items():
         command_parts.append(f"{key} {value}")
     modified_command = " \\\n    ".join(command_parts)
+    print(modified_command)
     return modified_command
 
 
