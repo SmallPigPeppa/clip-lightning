c1f112c93f2b7f07e2a4b7742d97a40c6c5ff019
lwz <905328134@qq.com>
2024-07-18 20:09:36 +0800

    update to lightning 2.0

--- main_continual_npu_distill.py
+++ main_continual_npu_distill.py
@@ -37,7 +37,7 @@ def read_base_command(file_path):
     return base_command, params_dict
 
 
-def modify_command_for_task(base_command, params_dict, task_idx, num_tasks):
+def modify_command_for_task(base_command, params_dict, task_idx, num_tasks, sub_dir):
     """
     Modify the command dictionary for a specific task index and convert it back to a command string.
     """
@@ -45,7 +45,6 @@ def modify_command_for_task(base_command, params_dict, task_idx, num_tasks):
     params_dict['--data.num_tasks'] = str(num_tasks)
     params_dict['--data.current_task'] = str(task_idx)
     ckpt_dir = params_dict['--model_checkpoint.dirpath']
-    sub_dir = generate_random_string_with_date(length=6)
     params_dict['--model_checkpoint.dirpath'] = os.path.join(ckpt_dir, sub_dir)
 
     # # Update ckpt path
@@ -75,9 +74,10 @@ def incremental_learning(num_tasks, script_path):
     Manage the incremental learning process across multiple tasks.
     """
     base_command, params_dict = read_base_command(script_path)
+    sub_dir = generate_random_string_with_date(length=6)
     for task_idx in range(num_tasks):
         print(f"Starting training for task {task_idx}")
-        task_specific_command = modify_command_for_task(base_command, params_dict.copy(), task_idx, num_tasks)
+        task_specific_command = modify_command_for_task(base_command, params_dict.copy(), task_idx, num_tasks, sub_dir)
         subprocess.run(task_specific_command, shell=True, check=True)
         print(f"Completed training for task {task_idx}")
 
