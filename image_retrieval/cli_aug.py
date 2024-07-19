d93a003335e6a51cf2580484af4770633823c7bb
lwz <905328134@qq.com>
2024-07-19 22:40:03 +0800

    update to lightning 2.0

--- image_retrieval/cli_aug.py
+++ image_retrieval/cli_aug.py
@@ -21,12 +21,12 @@ class CLI(cli.LightningCLI):
         )
 
         # log prediction
-        parser.add_lightning_class_args(
-            LogPredictionCallback, "log_prediction_callback"
-        )
-        parser.link_arguments(
-            "model.text_encoder_alias", "log_prediction_callback.tokenizer"
-        )
+        # parser.add_lightning_class_args(
+        #     LogPredictionCallback, "log_prediction_callback"
+        # )
+        # parser.link_arguments(
+        #     "model.text_encoder_alias", "log_prediction_callback.tokenizer"
+        # )
         parser.add_lightning_class_args(ModelCheckpoint, "model_checkpoint")
         parser.add_lightning_class_args(LearningRateMonitor, "lr_monitor")
 
