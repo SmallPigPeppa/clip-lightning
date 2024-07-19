572b3125711aa794fac128b312219ef208d1f0d4
lwz <905328134@qq.com>
2024-07-19 18:16:56 +0800

    update to lightning 2.0

--- image_retrieval/models/clip_model_distill_v3.py
+++ image_retrieval/models/clip_model_distill_v3.py
@@ -90,10 +90,12 @@ class CLIPDualEncoderModel(LightningModule):
         self.image_encoder_old = copy.deepcopy(self.image_encoder)
         self.text_encoder_old = copy.deepcopy(self.text_encoder)
 
-        for name, param in self.named_parameters():
-            if param.grad is None:
-                print(name)
-
+    def on_before_optimizer_step(self, optimizer) -> None:
+        print("on_before_opt enter")
+        for p in self.trainable_params:
+            if p.grad is None:
+                print(p)
+        print("on_before_opt exit")
     def forward(self, inputs):
         image_features = self.image_encoder(inputs["image"])
         text_features = self.text_encoder(
