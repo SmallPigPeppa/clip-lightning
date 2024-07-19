4ace7b3f2ca0b0864083369ac6a80c22eebbe82a
lwz <905328134@qq.com>
2024-07-20 01:11:36 +0800

    update to lightning 2.0

--- train_npu_r50_aug_bs256_debug_ILR_PLR_DILV2_distill_cassle.sh
+++ train_npu_r50_aug_bs256_debug_ILR_PLR_DILV2_distill_cassle.sh
@@ -22,7 +22,7 @@ python image_retrieval/cli_aug_dilv2_distill_cassle.py fit \
     --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger \
-    --trainer.logger.project CLIP-PL-DIL \
+    --trainer.logger.project CLIP-DIL \
     --trainer.logger.name r50-aug-bs256-debug-ILR-PLR-distill-cassle \
     --trainer.logger.log_model False \
     --lr_monitor.logging_interval epoch \
