016c3ed338fd9c02b84189d92fee016912cb8860
lwz <905328134@qq.com>
2024-07-19 16:34:16 +0800

    update to lightning 2.0

--- image_retrieval/models/clip_model_distill_v3.py
+++ image_retrieval/models/clip_model_distill_v3.py
@@ -72,12 +72,12 @@ class CLIPDualEncoderModel(LightningModule):
             dropout=self.hparams.dropout,
         )
 
-        if self.hparams.old_checkpoint_path:
-            checkpoint = torch.load(self.hparams.old_checkpoint_path, map_location=torch.device('cpu'))
-            filtered_state_dict = {k: v for k, v in checkpoint['state_dict'].items() if not k.startswith(
-                ('image_encoder_old', 'text_encoder_old', 'image_projection_old', 'text_projection_old'))}
-            self.load_state_dict(filtered_state_dict, strict=False)
-            print("Model weights loaded successfully and old parts copied.")
+        # load task N-1 checkpoint
+        checkpoint = torch.load(self.hparams.old_checkpoint_path, map_location=torch.device('cpu'))
+        filtered_state_dict = {k: v for k, v in checkpoint['state_dict'].items() if not k.startswith(
+            ('image_encoder_old', 'text_encoder_old', 'image_projection_old', 'text_projection_old'))}
+        self.load_state_dict(filtered_state_dict, strict=False)
+        print("Model weights loaded successfully and old parts copied.")
 
         self.image_encoder_old = copy.deepcopy(self.image_encoder)
         self.text_encoder_old = copy.deepcopy(self.text_encoder)
