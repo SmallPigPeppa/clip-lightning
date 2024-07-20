8025c2d5d49e696cbb2d054f4b6dcbcef5fd30a8
lwz <905328134@qq.com>
2024-07-20 13:18:03 +0800

    update to lightning 2.0

--- image_retrieval/models/clip_model_distill_cassle.py
+++ image_retrieval/models/clip_model_distill_cassle.py
@@ -69,7 +69,7 @@ class CLIPDualEncoderModel(LightningModule):
         checkpoint = torch.load(self.hparams.old_checkpoint_path, map_location=torch.device('cpu'))
         filtered_state_dict = {k: v for k, v in checkpoint['state_dict'].items() if not k.startswith(
             ('image_encoder_old', 'text_encoder_old', 'image_projection_old', 'text_projection_old'))}
-        self.load_state_dict(filtered_state_dict, strict=True)
+        self.load_state_dict(filtered_state_dict, strict=False)
         print("Model weights loaded successfully and old parts copied.")
 
         self.image_projection_old = copy.deepcopy(self.image_projection)
