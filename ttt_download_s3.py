66aa1e0a51b667cc495acdb4639b8f23f08f6fdb
lwz <905328134@qq.com>
2024-07-21 20:31:35 +0800

    update to lightning 2.0

--- ttt_download_s3.py
+++ ttt_download_s3.py
@@ -8,7 +8,7 @@ def copy_task(src, dst):
 
 # 定义源路径和目标路径
 src = 's3://bucket-5541/wenzhuoliu/torch_ds/cifar-100-python/'
-dst = '/home/ma-user/work/wenzhuoliu/torch_ds/cifar-100-python'
+dst = '/home/ma-user/work/wenzhuoliu/torch_ds/cifar100'
 
 # 使用 ThreadPoolExecutor 创建线程池
 with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
