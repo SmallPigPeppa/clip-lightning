ceba2da3eb0d0e1f4da57061bd40b1b3b779794b
lwz <905328134@qq.com>
2024-07-21 20:30:25 +0800

    update to lightning 2.0

---
+++ ttt_download_s3.py
@@ -0,0 +1,22 @@
+import concurrent.futures
+import moxing as mox
+
+
+def copy_task(src, dst):
+    mox.file.copy_parallel(src, dst)
+
+
+# 定义源路径和目标路径
+src = 's3://bucket-5541/wenzhuoliu/torch_ds/cifar-100-python/'
+dst = '/home/ma-user/work/wenzhuoliu/torch_ds/cifar-100-python'
+
+# 使用 ThreadPoolExecutor 创建线程池
+with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+    # 提交任务给线程池
+    futures = [executor.submit(copy_task, src, dst) for _ in range(16)]
+
+    # 等待所有任务完成
+    for future in concurrent.futures.as_completed(futures):
+        future.result()
+
+print("Copying completed.")
