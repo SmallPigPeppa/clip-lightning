2983dbfe66a1c2c02a578ae68a9c711e337a1389
lwz <905328134@qq.com>
2024-07-24 22:14:01 +0800

    update to lightning 2.0

--- clip_openai/dataloaders/base.py
+++ clip_openai/dataloaders/base.py
@@ -20,56 +20,60 @@ class ImageRetrievalDataset(Dataset):
     def __len__(self):
         return len(self.captions)
 
-    # def tokenize(self, text):
-    #     sot_token = self.tokenizer.encoder["<|startoftext|>"]
-    #     eot_token = self.tokenizer.encoder["<|endoftext|>"]
-    #     tokens = [sot_token] + self.tokenizer.encode(text) + [eot_token]
-    #     result = torch.zeros(self.max_length, dtype=torch.int)
-    #     if len(tokens) <= self.max_length:
-    #         result[:len(tokens)] = torch.tensor(tokens)
-    #     else:
-    #         result[:self.max_length] = torch.tensor(tokens)[:self.max_length]
-    #     return result
-
-    def tokenize(self, texts: Union[str, List[str]], truncate: bool = False) -> Union[
-        torch.IntTensor, torch.LongTensor]:
-        """
-        Returns the tokenized representation of given input string(s)
-
-        Parameters
-        ----------
-        texts : Union[str, List[str]]
-            An input string or a list of input strings to tokenize
-
-        truncate: bool
-            Whether to truncate the text in case its encoding is longer than the context length
-
-        Returns
-        -------
-        A two-dimensional tensor containing the resulting tokens, shape = [number of input strings, context_length].
-        We return LongTensor when torch version is <1.8.0, since older index_select requires indices to be long.
-        """
-        if isinstance(texts, str):
-            texts = [texts]
-
-        sot_token = self.tokenizer.encode["<|startoftext|>"]
-        eot_token = self.tokenizer.encode["<|endoftext|>"]
-        all_tokens = [[sot_token] + self.tokenizer.encode(text) + [eot_token] for text in texts]
+    def tokenize(self, text):
+        sot_token = self.tokenizer.encoder["<|startoftext|>"]
+        eot_token = self.tokenizer.encoder["<|endoftext|>"]
+        tokens = [sot_token] + self.tokenizer.encode(text) + [eot_token]
         if version.parse(torch.__version__) < version.parse("1.8.0"):
-            result = torch.zeros(len(all_tokens), self.max_length, dtype=torch.long)
+            result = torch.zeros(self.max_length, dtype=torch.long)
         else:
-            result = torch.zeros(len(all_tokens), self.max_length, dtype=torch.int)
+            result = torch.zeros(self.max_length, dtype=torch.int)
 
-        for i, tokens in enumerate(all_tokens):
-            if len(tokens) > self.max_length:
-                if truncate:
-                    tokens = tokens[:self.max_length]
-                    tokens[-1] = eot_token
-                else:
-                    raise RuntimeError(f"Input {texts[i]} is too long for context length {self.max_length}")
-            result[i, :len(tokens)] = torch.tensor(tokens)
-
+        if len(tokens) <= self.max_length:
+            result[:len(tokens)] = torch.tensor(tokens)
+        else:
+            result[:self.max_length] = torch.tensor(tokens)[:self.max_length]
         return result
+
+    # def tokenize(self, texts: Union[str, List[str]], truncate: bool = False) -> Union[
+    #     torch.IntTensor, torch.LongTensor]:
+    #     """
+    #     Returns the tokenized representation of given input string(s)
+    #
+    #     Parameters
+    #     ----------
+    #     texts : Union[str, List[str]]
+    #         An input string or a list of input strings to tokenize
+    #
+    #     truncate: bool
+    #         Whether to truncate the text in case its encoding is longer than the context length
+    #
+    #     Returns
+    #     -------
+    #     A two-dimensional tensor containing the resulting tokens, shape = [number of input strings, context_length].
+    #     We return LongTensor when torch version is <1.8.0, since older index_select requires indices to be long.
+    #     """
+    #     if isinstance(texts, str):
+    #         texts = [texts]
+    #
+    #     sot_token = self.tokenizer.encode["<|startoftext|>"]
+    #     eot_token = self.tokenizer.encode["<|endoftext|>"]
+    #     all_tokens = [[sot_token] + self.tokenizer.encode(text) + [eot_token] for text in texts]
+    #     if version.parse(torch.__version__) < version.parse("1.8.0"):
+    #         result = torch.zeros(len(all_tokens), self.max_length, dtype=torch.long)
+    #     else:
+    #         result = torch.zeros(len(all_tokens), self.max_length, dtype=torch.int)
+    #
+    #     for i, tokens in enumerate(all_tokens):
+    #         if len(tokens) > self.max_length:
+    #             if truncate:
+    #                 tokens = tokens[:self.max_length]
+    #                 tokens[-1] = eot_token
+    #             else:
+    #                 raise RuntimeError(f"Input {texts[i]} is too long for context length {self.max_length}")
+    #         result[i, :len(tokens)] = torch.tensor(tokens)
+    #
+    #     return result
 
     def __getitem__(self, index):
         image = Image.open(self.images[index])
