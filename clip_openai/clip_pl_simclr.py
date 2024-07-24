25a25d3105d2a264f1f7271e3daa963bdc208f9a
lwz <905328134@qq.com>
2024-07-24 21:32:44 +0800

    update to lightning 2.0

--- clip_openai/clip_pl_simclr.py
+++ clip_openai/clip_pl_simclr.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from lightning import LightningModule
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
-from .model import my_load
+from .model_openai import my_load
 import copy
 
 
@@ -183,23 +183,3 @@ class CLIPDualEncoderModel(LightningModule):
         return metrics
 
 
-if __name__ == "__main__":
-    from clip_openai.model import my_load
-    from dataloaders import ImageRetrievalDataModule
-
-    dm = ImageRetrievalDataModule(
-        dataset_name='flickr30k',
-        config='../config.yaml',
-        root_dir='../artifacts/flickr-30k:v0',
-        val_split=0.2,
-        max_length=77
-    )
-    dm.setup(stage='fit')
-    a = dm.train_dataloader()
-    inputs = next(iter(a))
-
-    model = my_load(name='RN50', download_root='./')
-    image_features = model.encode_image(inputs["image"])
-    text_features = model.encode_text(inputs["caption"])
-    print(image_features.shape)
-    print(text_features.shape)
--- clip_openai/debug.py
+++ clip_openai/debug.py
@@ -1,13 +1,23 @@
 from dataloaders import Flickr30kDataset
-from clip_openai.model.simple_tokenizer import SimpleTokenizer
+from clip_openai.model_openai.simple_tokenizer import SimpleTokenizer
 
 if __name__ == "__main__":
-    tokenizer = SimpleTokenizer()
-    dataset = Flickr30kDataset(
+    from clip_openai.model_openai import my_load
+    from dataloaders import ImageRetrievalDataModule
+
+    dm = ImageRetrievalDataModule(
+        dataset_name='flickr30k',
+        config='../config.yaml',
         root_dir='../artifacts/flickr-30k:v0',
-        tokenizer=tokenizer,
+        val_split=0.2,
+        max_length=77
     )
-    inputs = dataset[0]
-    print(a)
+    dm.setup(stage='fit')
+    a = dm.train_dataloader()
+    inputs = next(iter(a))
 
-
+    model = my_load(name='RN50', download_root='./')
+    image_features = model.encode_image(inputs["image"])
+    text_features = model.encode_text(inputs["caption"])
+    print(image_features.shape)
+    print(text_features.shape)
