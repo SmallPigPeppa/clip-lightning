737c6d0f37dbb2f0b5cb58b334d65d155120066f
lwz <905328134@qq.com>
2024-07-25 15:32:53 +0800

    update to lightning 2.0

---
+++ clip_reproduce/model_reproduce/encoders/text_encoder.py
@@ -0,0 +1,32 @@
+import torch
+import transformers
+from torch import nn
+# from transformers import DistilBertTokenizer, DistilBertModel
+from .modeling_distillbert_plv2 import DistilBertModel
+import lightning as pl
+
+# tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+# model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+
+
+# class TextEncoder(nn.Module):
+class TextEncoder(pl.LightningModule):
+    def __init__(self, model_name: str, trainable: bool = True) -> None:
+        super().__init__()
+
+        # self.model = transformers.AutoModel.from_pretrained(model_name)
+        self.model = DistilBertModel.from_pretrained(model_name)
+
+        for param in self.model.parameters():
+            param.requires_grad = trainable
+
+        self.target_token_idx = 0
+
+    def forward(self, input_ids, attention_mask):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        last_hidden_state = output.last_hidden_state
+
+        return last_hidden_state[:, self.target_token_idx, :]
+
+
+
