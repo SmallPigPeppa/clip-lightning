57d953313fd880a894131452cb9193fea32e72eb
lwz <905328134@qq.com>
2024-07-25 17:11:37 +0800

    update to lightning 2.0

--- clip_reproduce/train_r50_cassle.sh
+++ clip_reproduce/train_r50_cassle.sh
@@ -18,7 +18,7 @@ python cli_cassle.py fit \
     --model.text_embedding_dims 768 \
     --model.text_encoder_lr 1e-4 \
     --model.projection_dims 1024 \
-    --trainer.accelerator gpu \
+    --trainer.accelerator npu \
     --trainer.precision 16 \
     --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
