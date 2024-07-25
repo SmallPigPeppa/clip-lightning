ae22a28768a9158fbca0d9b700387219e74fbadd
lwz <905328134@qq.com>
2024-07-25 16:07:59 +0800

    update to lightning 2.0

--- clip_reproduce/train_r50_vanilla.sh
+++ clip_reproduce/train_r50_vanilla.sh
@@ -18,7 +18,7 @@ python cli_vanilla.py fit \
     --model.text_embedding_dims 768 \
     --model.text_encoder_lr 1e-5 \
     --model.projection_dims 1024 \
-    --trainer.accelerator npu \
+    --trainer.accelerator gpu \
     --trainer.precision 16 \
     --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
