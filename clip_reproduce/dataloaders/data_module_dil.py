41bd560946541b365ade8c9ce93a8acdd2933dd4
lwz <905328134@qq.com>
2024-07-25 15:54:06 +0800

    update to lightning 2.0

---
+++ clip_reproduce/dataloaders/data_module_dil.py
@@ -0,0 +1,77 @@
+from typing import Optional
+from torch.utils.data import DataLoader, Subset
+from lightning import LightningDataModule
+from transformers import DistilBertTokenizer
+from .flickr30k import Flickr30kDataset
+from .img_transforms import image_transform_v2
+
+DATASET_LOOKUP = {"flickr30k": Flickr30kDataset}
+
+class ImageRetrievalDataModule(LightningDataModule):
+    def __init__(
+            self,
+            dataset_name: str,
+            config: str,
+            root_dir: str = None,
+            tokenizer_alias: Optional[str] = None,
+            max_length: int = 77,
+            batch_size: int = 16,
+            num_workers: int = 8,
+            num_tasks: int = 1,
+            current_task: int = 0,
+            *args,
+            **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.dataset_name = dataset_name
+        self.config = config
+        self.root_dir = root_dir
+        self.batch_size = batch_size
+        self.tokenizer = DistilBertTokenizer.from_pretrained(tokenizer_alias)
+        self.max_length = max_length
+        self.num_workers = num_workers
+        self.num_tasks = num_tasks
+        self.current_task = current_task
+
+
+    def setup(
+            self,
+            stage: Optional[str] = None,
+    ) -> None:
+        train_dataset = DATASET_LOOKUP[self.dataset_name](
+            root_dir=self.root_dir,
+            tokenizer=self.tokenizer,
+            max_length=self.max_length,
+            split="train",
+            transforms=image_transform_v2(config_path=self.config, is_train=True)
+        )
+        self.val_dataset = DATASET_LOOKUP[self.dataset_name](
+            root_dir=self.root_dir,
+            tokenizer=self.tokenizer,
+            max_length=self.max_length,
+            split="val",
+            transforms=image_transform_v2(config_path=self.config, is_train=False)
+        )
+
+        # 划分训练集为多个任务
+        task_size = len(train_dataset) // self.num_tasks
+        self.task_datasets = [Subset(train_dataset, range(i * task_size, (i + 1) * task_size)) for i in
+                              range(self.num_tasks)]
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.task_datasets[self.current_task],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            drop_last=True,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
