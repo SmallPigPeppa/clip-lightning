a40c082e0ca7a3cddc4c223d5118e6051507ecc7
lwz <905328134@qq.com>
2024-07-26 01:16:59 +0800

    update to lightning 2.0

--- clip_reproduce/train_r50_simclr_debug.sh
+++ clip_reproduce/train_r50_simclr_debug.sh
@@ -12,14 +12,14 @@ python cli_simclr.py fit \
     --model.image_encoder_pretrained True \
     --model.image_encoder_trainable True \
     --model.image_embedding_dims 2048 \
-    --model.image_encoder_lr 5e-4 \
-    --model.head_lr 5e-4 \
+    --model.image_encoder_lr 1e-3 \
+    --model.head_lr 1e-3 \
     --model.text_encoder_trainable True \
     --model.text_embedding_dims 768 \
-    --model.text_encoder_lr 5e-4 \
+    --model.text_encoder_lr 1e-3 \
     --model.projection_dims 1024 \
     --model.weight_decay 0.2 \
-    --model.temperature 1.0 \
+    --model.temperature 0.07 \
     --trainer.accelerator npu \
     --trainer.precision 16 \
     --trainer.max_epochs 40 \
