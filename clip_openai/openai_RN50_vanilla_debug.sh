13d5655264b4918270c5007d626ef66de96a46c0
lwz <905328134@qq.com>
2024-07-26 19:51:32 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_debug.sh
+++ clip_openai/openai_RN50_vanilla_debug.sh
@@ -2,7 +2,7 @@ python cli_vanilla.py fit \
     --data.num_tasks 1 \
     --data.current_task 0 \
     --data.max_length 77 \
-    --data.batch_size 128 \
+    --data.batch_size 256 \
     --data.num_workers 8 \
     --data.config config.yaml \
     --data.dataset_name flickr30k \
@@ -10,7 +10,7 @@ python cli_vanilla.py fit \
     --model.model_name RN50 \
     --model.projection_dims 1024 \
     --model.temperature 0.1 \
-    --model.lr 1e-5 \
+    --model.lr 1e-4 \
     --model.lr_warmup_epochs 5 \
     --model.weight_decay 0.2 \
     --model.download_root ./ \
