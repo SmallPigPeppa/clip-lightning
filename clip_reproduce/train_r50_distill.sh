a00394d60efd051affc068e779eddb6c386e76cd
lwz <905328134@qq.com>
2024-07-26 13:40:55 +0800

    update to lightning 2.0

--- clip_reproduce/train_r50_distill.sh
+++ clip_reproduce/train_r50_distill.sh
@@ -19,9 +19,9 @@ python cli_distill.py fit \
     --model.text_encoder_lr 5e-4 \
     --model.projection_dims 1024 \
     --model.weight_decay 0.2 \
-    --trainer.accelerator npu \
+    --trainer.accelerator gpu \
     --trainer.precision 16 \
-    --trainer.max_epochs 40 \
+    --trainer.max_epochs 1 \
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger \
     --trainer.logger.project CLIP-DIL-Reproduce \
