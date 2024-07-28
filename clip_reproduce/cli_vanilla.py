224d89cc7f690d2f00aa528c976284f465886eee
lwz <905328134@qq.com>
2024-07-28 19:36:13 +0800

    update to lightning 2.0

--- clip_reproduce/cli_vanilla.py
+++ clip_reproduce/cli_vanilla.py
@@ -8,6 +8,8 @@ import os
 
 os.environ['CURL_CA_BUNDLE'] = ''
 
+import torch
+torch.set_float32_matmul_precision('medium' | 'high')
 
 class CLI(cli.LightningCLI):
     def add_arguments_to_parser(self, parser: cli.LightningArgumentParser) -> None:
