e45ec3841af5294713e1201ffd403d0b9b6bec7c
lwz <905328134@qq.com>
2024-07-28 19:27:10 +0800

    update to lightning 2.0

--- clip_reproduce/train_r50_vanilla_coco.sh
+++ clip_reproduce/train_r50_vanilla_coco.sh
@@ -6,7 +6,7 @@ python cli_vanilla.py fit \
     --data.num_workers 8 \
     --data.config config.yaml \
     --data.dataset_name coco2014 \
-    --data.root_dir /mnt/ShareDB_1TB/datasets/coco_caption \
+    --data.root_dir /ppio_net0/torch_ds/coco \
     --model.image_encoder_alias resnet50 \
     --model.text_encoder_alias distilbert-base-uncased \
     --model.image_encoder_pretrained True \
