1a54bc9b83772d8e034dea3baddb2bce19d31be8
lwz <905328134@qq.com>
2024-07-28 18:59:50 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla.sh
+++ clip_openai/openai_RN50_vanilla.sh
@@ -14,7 +14,7 @@ python cli_vanilla.py fit \
     --model.lr_warmup_epochs 5 \
     --model.weight_decay 0. \
     --model.download_root ./ \
-    --trainer.accelerator gpu \
+    --trainer.accelerator npu \
     --trainer.precision 16 \
     --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
