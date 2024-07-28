00ff1b0cbd63844f4b8249fdfd8b599cb608ff9f
lwz <905328134@qq.com>
2024-07-28 22:15:51 +0800

    update to lightning 2.0

--- clip_reproduce/train_r50_vanilla_coco_pal.sh
+++ clip_reproduce/train_r50_vanilla_coco_pal.sh
@@ -27,7 +27,6 @@ python cli_vanilla.py fit \
     --trainer.logger.project CLIP-coco2014 \
     --trainer.logger.name RN50-vanilla \
     --trainer.logger.log_model False \
-    --trainer.logger.offline True \
     --lr_monitor.logging_interval epoch \
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
