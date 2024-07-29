43710b4cb2600cd3c0701e18381d27cc25709562
lwz <905328134@qq.com>
2024-07-30 01:02:38 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_cub200.sh
+++ clip_openai/openai_RN50_vanilla_cub200.sh
@@ -2,7 +2,7 @@ python cli_vanilla.py fit \
     --data.num_tasks 1 \
     --data.current_task 0 \
     --data.max_length 77 \
-    --data.batch_size 128 \
+    --data.batch_size 256 \
     --data.num_workers 8 \
     --data.config config.yaml \
     --data.dataset_name cub200 \
@@ -10,9 +10,9 @@ python cli_vanilla.py fit \
     --model.model_name RN50 \
     --model.projection_dims 1024 \
     --model.temperature 0.1 \
-    --model.lr 1e-5 \
+    --model.lr 1e-4 \
     --model.lr_warmup_epochs 5 \
-    --model.weight_decay 0. \
+    --model.weight_decay 0.2 \
     --model.download_root ./ \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
@@ -25,4 +25,4 @@ python cli_vanilla.py fit \
     --lr_monitor.logging_interval epoch \
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
-    --model_checkpoint.filename  RN50-vanilla
+    --model_checkpoint.filename  RN50-vanilla-pretrain
