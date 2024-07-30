515a6106cc76bc163c0a233e891bc58c0c42cfd1
lwz <905328134@qq.com>
2024-07-30 12:56:54 +0800

    update to lightning 2.0

--- clip_reproduce/train_r50_vanilla_cub.sh
+++ clip_reproduce/train_r50_vanilla_cub.sh
@@ -6,7 +6,7 @@ python cli_vanilla.py fit \
     --data.num_workers 8 \
     --data.config config.yaml \
     --data.dataset_name cub200 \
-    --data.root_dir /ppio_net0/torch_ds/cub200 \
+    --data.root_dir /mnt/ShareDB_1TB/datasets/cub200 \
     --model.image_encoder_alias resnet50 \
     --model.text_encoder_alias distilbert-base-uncased \
     --model.image_encoder_pretrained True \
