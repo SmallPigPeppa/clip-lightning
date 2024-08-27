377a8b9c2669a94abc40d85a524f99b4556eecc5
lwz <905328134@qq.com>
2024-08-28 00:59:44 +0800

    update to lightning 2.0

--- clip_openai/zero_shot/zero_shot_classifier.py
+++ clip_openai/zero_shot/zero_shot_classifier.py
@@ -39,7 +39,7 @@ class ZeroShotClassifier(LightningModule):
         def _process_batch(batch_classnames):
             texts = [template.format(c) if use_format else template(c) for c in batch_classnames for template in
                      self.templates]
-            texts = self.tokenizer(texts).to(self.device)
+            texts = self.tokenizer.encode(texts).to(self.device)
             class_embeddings = self.model.encode_text(texts)
             class_embeddings = class_embeddings.reshape(len(batch_classnames), num_templates, -1).mean(dim=1)
             class_embeddings = class_embeddings / class_embeddings.norm(dim=1, keepdim=True)
