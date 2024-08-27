66d1ca8ef02567a8b2d47a7134fdd621bdc22303
lwz <905328134@qq.com>
2024-08-28 00:54:57 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot.sh
@@ -6,7 +6,7 @@ python cli_vanilla_zeroshot.py validate \
     --data.num_workers 8 \
     --data.config config.yaml \
     --data.dataset_name flickr30k \
-    --data.root_dir ../artifacts/flickr-30k:v0/flickr30k_images \
+    --data.root_dir /ppio_net0/torch_ds \
     --model.model_name RN50 \
     --model.projection_dims 1024 \
     --model.temperature 0.1 \
