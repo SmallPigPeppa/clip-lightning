2c5f43f13071255b7e017f96a618f885da68e99b
lwz <905328134@qq.com>
2024-08-27 08:30:34 +0800

    update to lightning 2.0

--- clip_openai/dataloaders/data_module_dil_json.py
+++ clip_openai/dataloaders/data_module_dil_json.py
@@ -7,10 +7,12 @@ from .flickr30k_json import Flickr30kDataset
 from .img_transforms import image_transform_v2
 from model_openai import SimpleTokenizer
 from .cub200 import CUB200Dataset
+from .food import UPMCFood101Dataset
 
 DATASET_LOOKUP = {
     'flickr30k': Flickr30kDataset,
-    'cub200': CUB200Dataset
+    'cub200': CUB200Dataset,
+    'food': UPMCFood101Dataset
 }
 
 
