e82352150754ae6e76fd1da5cc4e612ef1c4f01a
lwz <905328134@qq.com>
2024-08-28 23:48:14 +0800

    update to lightning 2.0

--- clip_openai/cli_distill_zeroshot.py
+++ clip_openai/cli_distill_zeroshot.py
@@ -1,7 +1,7 @@
 from lightning.pytorch import cli
 from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from lightning.pytorch.callbacks.lr_monitor import LearningRateMonitor
-from dataloaders.data_module_dil_json import ImageRetrievalDataModule
+from dataloaders.data_module_dil_json_zeroshot import ImageRetrievalDataModule
 from model_distill import CLIPDualEncoderModel
 
 import os
