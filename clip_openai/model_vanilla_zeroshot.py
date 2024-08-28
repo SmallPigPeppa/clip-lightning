4bab16727e60f98faa92c0209ef5f07e8341a3e0
lwz <905328134@qq.com>
2024-08-28 21:54:49 +0800

    update to lightning 2.0

--- clip_openai/model_vanilla_zeroshot.py
+++ clip_openai/model_vanilla_zeroshot.py
@@ -164,7 +164,7 @@ class CLIPDualEncoderModel(LightningModule):
             classnames=IMAGENET_CLASSNAMES,
             templates=OPENAI_IMAGENET_TEMPLATES,
             num_classes_per_batch=self.hparams.batch_size,
-        ).to(self.device)
+        ).to(self.device).eval()
 
         self.zero_shot_classifier.compute_weights()
 
