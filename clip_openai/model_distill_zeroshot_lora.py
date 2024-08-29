6e5f84ccfaa138dcb3d64aba1d4077ca374a4e61
lwz <905328134@qq.com>
2024-08-30 03:10:11 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_lora.py
+++ clip_openai/model_distill_zeroshot_lora.py
@@ -34,8 +34,8 @@ def get_lora_model(model):
     # Initialize LoRA configuration with target modules
     lora_config = LoraConfig(
         inference_mode=False,
-        r=16,  # Rank of the low-rank decomposition
-        lora_alpha=32,  # Scaling factor
+        r=64,  # Rank of the low-rank decomposition
+        lora_alpha=64,  # Scaling factor
         task_type=TaskType.SEQ_CLS,  # Task type
         lora_dropout=0.1,  # Dropout rate for LoRA
         target_modules=target_modules  # Specify the target modules
@@ -54,8 +54,8 @@ def get_lora_model2(model):
     # Initialize LoRA configuration with target modules
     lora_config = LoraConfig(
         inference_mode=False,
-        r=16,  # Rank of the low-rank decomposition
-        lora_alpha=32,  # Scaling factor
+        r=64,  # Rank of the low-rank decomposition
+        lora_alpha=64,  # Scaling factor
         task_type='vision',  # Task type
         lora_dropout=0.1,  # Dropout rate for LoRA
         target_modules=target_modules  # Specify the target modules
@@ -76,12 +76,10 @@ class DistillPredictor(nn.Module):
         self.pd_linear2 = nn.Linear(distill_proj_hidden_dim, projection_dims)
 
     def forward(self, x):
-        residual = x  # 保存输入以用于残差连接
         x = self.pd_linear1(x)
         x = self.pd_batch_norm(x)
         x = self.pd_relu(x)
         x = self.pd_linear2(x)
-        x += residual  # 加上残差连接
         return x
 
 
