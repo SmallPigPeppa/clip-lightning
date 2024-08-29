62db76bec5c2c9d05853e0e2fa2d1eeedf42b5d0
lwz <905328134@qq.com>
2024-08-29 15:30:06 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_lora.py
+++ clip_openai/model_distill_zeroshot_lora.py
@@ -18,6 +18,14 @@ from peft import get_peft_model, LoraConfig, TaskType
 from transformers.pytorch_utils import Conv1D
 
 
+# def find_target_modules(model):
+#     target_modules = []
+#     for name, module in model.named_modules():
+#         if isinstance(module, (nn.Linear, nn.Embedding, nn.Conv2d, Conv1D)):
+#             target_modules.append(name)
+#     return target_modules
+
+
 def find_target_modules(model):
     target_modules = []
     for module in model.modules():
