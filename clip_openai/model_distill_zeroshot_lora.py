78108e7d34e7d13642e2fc83be41b570dda1ae76
lwz <905328134@qq.com>
2024-08-30 15:36:43 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_lora.py
+++ clip_openai/model_distill_zeroshot_lora.py
@@ -34,8 +34,8 @@ def get_lora_model(model):
     # Initialize LoRA configuration with target modules
     lora_config = LoraConfig(
         inference_mode=False,
-        r=16,  # Rank of the low-rank decomposition
-        lora_alpha=32,  # Scaling factor
+        r=512,  # Rank of the low-rank decomposition
+        lora_alpha=512,  # Scaling factor
         task_type=TaskType.SEQ_CLS,  # Task type
         lora_dropout=0.1,  # Dropout rate for LoRA
         target_modules=target_modules  # Specify the target modules
@@ -47,24 +47,24 @@ def get_lora_model(model):
     return lora_model
 
 
-def get_lora_model2(model):
-    # Define the target modules where LoRA should be applied
-    target_modules = find_target_modules(model)
-
-    # Initialize LoRA configuration with target modules
-    lora_config = LoraConfig(
-        inference_mode=False,
-        r=16,  # Rank of the low-rank decomposition
-        lora_alpha=32,  # Scaling factor
-        task_type='vision',  # Task type
-        lora_dropout=0.1,  # Dropout rate for LoRA
-        target_modules=target_modules  # Specify the target modules
-    )
-
-    # Apply LoRA to the model
-    lora_model = get_peft_model(model, lora_config)
-
-    return lora_model
+# def get_lora_model2(model):
+#     # Define the target modules where LoRA should be applied
+#     target_modules = find_target_modules(model)
+#
+#     # Initialize LoRA configuration with target modules
+#     lora_config = LoraConfig(
+#         inference_mode=False,
+#         r=16,  # Rank of the low-rank decomposition
+#         lora_alpha=32,  # Scaling factor
+#         task_type='vision',  # Task type
+#         lora_dropout=0.1,  # Dropout rate for LoRA
+#         target_modules=target_modules  # Specify the target modules
+#     )
+#
+#     # Apply LoRA to the model
+#     lora_model = get_peft_model(model, lora_config)
+#
+#     return lora_model
 
 
 class DistillPredictor(nn.Module):
