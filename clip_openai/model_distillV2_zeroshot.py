1500e328dfffeeeeda2a0a51823d774321d2308b
lwz <905328134@qq.com>
2024-08-31 00:10:46 +0800

    update to lightning 2.0

--- clip_openai/model_distillV2_zeroshot.py
+++ clip_openai/model_distillV2_zeroshot.py
@@ -192,9 +192,14 @@ class CLIPDualEncoderModel(LightningModule):
             p1 = self.distill_predictor(image_embeddings)
             p2 = self.distill_predictor(text_embeddings)
 
+            # distill_loss = (
+            #                        self.simclr_distill_loss_func(p1, p2, frozen_z1, frozen_z2)
+            #                        + self.simclr_distill_loss_func(frozen_z1, frozen_z2, p1, p2)
+            #                ) / 2
+
             distill_loss = (
-                                   self.simclr_distill_loss_func(p1, p2, frozen_z1, frozen_z2)
-                                   + self.simclr_distill_loss_func(frozen_z1, frozen_z2, p1, p2)
+                                   self._compute_losses(p1, frozen_z1)
+                                   + self._compute_losses(p2, frozen_z2)
                            ) / 2
 
             self.log("train/distill_loss", distill_loss, sync_dist=True)
@@ -214,10 +219,14 @@ class CLIPDualEncoderModel(LightningModule):
             p1 = self.distill_predictor(image_embeddings)
             p2 = self.distill_predictor(text_embeddings)
 
+            # distill_loss = (
+            #                        self.simclr_distill_loss_func(p1, p2, frozen_z1, frozen_z2)
+            #                        + self.simclr_distill_loss_func(frozen_z1, frozen_z2, p1, p2)
+            #                ) / 2
             distill_loss = (
-                                   self.simclr_distill_loss_func(p1, p2, frozen_z1, frozen_z2)
-                                   + self.simclr_distill_loss_func(frozen_z1, frozen_z2, p1, p2)
-                           ) / 2 * 1000.
+                                   self._compute_losses(p1, frozen_z1)
+                                   + self._compute_losses(p2, frozen_z2)
+                           ) / 2
             self.log("val/distill_loss", distill_loss, sync_dist=True)
             return clip_loss + distill_loss
         else:
