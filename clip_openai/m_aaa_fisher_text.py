705351950198b8a55868b5ae727f06ad44c0796f
lwz <905328134@qq.com>
2024-09-01 22:13:06 +0800

    update to lightning 2.0

--- clip_openai/m_aaa_fisher_text.py
+++ clip_openai/m_aaa_fisher_text.py
@@ -201,7 +201,7 @@ if __name__ == '__main__':
     model = YourLightningModule(
         model_name='ViT-B/16',
         download_root='./',
-        num_classes_per_batch=50,
+        num_classes_per_batch=20,
         lr=0.,
         max_length=77,
         batch_size=128,
