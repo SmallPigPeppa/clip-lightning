10c9c34b8ddc41d71a9fd2fbb9f5b905f83e82f5
lwz <905328134@qq.com>
2024-09-02 00:46:37 +0800

    initial

--- clip_openai/model_distill_zeroshot_lora.py
+++ clip_openai/model_distill_zeroshot_lora.py
@@ -254,11 +254,11 @@ class CLIPDualEncoderModel(LightningModule):
         # for param in self.model.transformer.parameters():
         #     param.requires_grad = False
 
-        # for param in self.model.token_embedding.parameters():
-        #     param.requires_grad = False
-        #
-        # # 冻结 positional_embedding 参数
-        # self.model.positional_embedding.requires_grad = False
+        for param in self.model.token_embedding.parameters():
+            param.requires_grad = False
+
+        # 冻结 positional_embedding 参数
+        self.model.positional_embedding.requires_grad = False
 
         # # 冻结 ln_final 参数
         # for param in self.model.ln_final.parameters():
