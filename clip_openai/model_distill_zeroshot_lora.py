da1fc75f11b0860edd1cd1055814fea371c5c5a7
lwz <905328134@qq.com>
2024-09-01 19:46:07 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_lora.py
+++ clip_openai/model_distill_zeroshot_lora.py
@@ -212,18 +212,18 @@ class CLIPDualEncoderModel(LightningModule):
             if name in exact_layers_to_freeze or any(f"resblocks.{i}" in name for i in range(9)):
                 param.requires_grad = False
 
-        for param in self.model.transformer.parameters():
-            param.requires_grad = False
-
-        for param in self.model.token_embedding.parameters():
-            param.requires_grad = False
-
-        # 冻结 positional_embedding 参数
-        self.model.positional_embedding.requires_grad = False
-
-        # 冻结 ln_final 参数
-        for param in self.model.ln_final.parameters():
-            param.requires_grad = False
+        # for param in self.model.transformer.parameters():
+        #     param.requires_grad = False
+        #
+        # for param in self.model.token_embedding.parameters():
+        #     param.requires_grad = False
+        #
+        # # 冻结 positional_embedding 参数
+        # self.model.positional_embedding.requires_grad = False
+        #
+        # # 冻结 ln_final 参数
+        # for param in self.model.ln_final.parameters():
+        #     param.requires_grad = False
 
     def initialize_old_modules(self):
         self.model_old = copy.deepcopy(self.model)
