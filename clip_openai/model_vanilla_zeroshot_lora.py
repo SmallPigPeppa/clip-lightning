a56dcbd62f805fc59a1ddc9ffb8dfbc4cb696524
lwz <905328134@qq.com>
2024-09-02 02:33:08 +0800

    initial

--- clip_openai/model_vanilla_zeroshot_lora.py
+++ clip_openai/model_vanilla_zeroshot_lora.py
@@ -53,10 +53,10 @@ class CLIPDualEncoderModel(LightningModule):
         lora_config = LoraConfig(
             r=16,  # Rank of the low-rank decomposition
             lora_alpha=32,  # Scaling factor
-            # task_type=TaskType.SEQ_CLS,  # Task type
+            task_type=TaskType.SEQ_CLS,  # Task type
             lora_dropout=0.1,  # Dropout rate for LoRA
-            # target_modules=target_modules,  # Specify the target modules
-            target_modules=['transformer','visual']
+            target_modules=target_modules,  # Specify the target modules
+            # target_modules=['transformer','visual']
         )
 
         # Apply LoRA to the model
