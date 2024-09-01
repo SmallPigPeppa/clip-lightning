725c9d64354790070d7fb72cc99fe303e820fd10
lwz <905328134@qq.com>
2024-09-01 22:21:28 +0800

    update to lightning 2.0

--- clip_openai/m_aaa_fisher_text.py
+++ clip_openai/m_aaa_fisher_text.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
         num_classes_per_batch=20,
         lr=0.,
         max_length=77,
-        batch_size=32,
+        batch_size=16,
         num_workers=8,
         root_dir='/ppio_net0/torch_ds',
         fisher_dir='./fisher_data',
