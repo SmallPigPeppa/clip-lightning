0ed4e5a454679046a346a56bae7eff33912d0e72
lwz <905328134@qq.com>
2024-09-01 21:27:58 +0800

    update to lightning 2.0

--- clip_openai/m_aaa_fisher.py
+++ clip_openai/m_aaa_fisher.py
@@ -157,7 +157,7 @@ class YourLightningModule(pl.LightningModule):
         print(self.fisher)
         # Optionally, save fisher information after each epoch
         os.makedirs(self.hparams.fisher_dir, exist_ok=True)
-        fisher_save_path = os.path.join(self.hparams.fisher_dir, f"fisher_epoch_{self.current_epoch}.pth")
+        fisher_save_path = os.path.join(self.hparams.fisher_dir, f"{self.hparams.model_name}_fisher_epoch_{self.current_epoch}.pth")
 
         # Save the Fisher information
         torch.save(self.fisher, fisher_save_path)
