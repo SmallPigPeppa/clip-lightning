515aafd7d2a76a73b9c7c169dfb6ec97d1aec6b0
lwz <905328134@qq.com>
2024-09-11 01:54:17 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf.sh
@@ -17,7 +17,7 @@ python cli_vanilla_zeroshot_hf.py fit \
     --model.temperature 0.1 \
     --model.lr 1e-5 \
     --model.lr_warmup_epochs 5 \
-    --model.weight_decay 0. \
+    --model.weight_decay 0.1 \
     --model.download_root ./ \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
