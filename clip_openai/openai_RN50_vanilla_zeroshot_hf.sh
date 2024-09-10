fb058f6d43fdf83b02317c20b9a3478bbb729740
lwz <905328134@qq.com>
2024-09-11 01:34:07 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf.sh
@@ -7,7 +7,7 @@ python cli_vanilla_zeroshot_hf.py fit \
     --data.current_task 0 \
     --data.max_length 77 \
     --data.batch_size 128 \
-    --data.batch_size_zs 128 \
+    --data.batch_size_zs 32 \
     --data.num_workers 8 \
     --data.config config.yaml \
     --data.dataset_name ${DATASET_NAME} \
