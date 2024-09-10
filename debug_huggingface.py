ae919269e0e4d649a2d73f2f01b834205cbb273c
lwz <905328134@qq.com>
2024-09-10 22:54:43 +0800

    update to lightning 2.0

--- debug_huggingface.py
+++ debug_huggingface.py
@@ -8,7 +8,9 @@ os.environ['CURL_CA_BUNDLE'] = ''
 
 ds = load_dataset("UCSC-VLAA/Recap-COCO-30K")
 ds = load_dataset("Tverous/flicker30k")
+ds = load_dataset("phiyodr/coco2017")
 ds = load_dataset("jinaai/fashion-captions-de")
 ds = load_dataset("OpenFace-CQUPT/FaceCaption-15M")
 ds = load_dataset("lcolonn/patfig")
 ds = load_dataset("jmhessel/newyorker_caption_contest", "explanation")
+ds = load_dataset("AterMors/wikiart_recaption")
