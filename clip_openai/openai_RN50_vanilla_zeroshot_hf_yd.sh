d72b2c0954c458949a5e61a9a03d1fbe9d761ed4
lwz <905328134@qq.com>
2024-09-11 16:53:03 +0800

    update to lightning 2.0

---
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_yd.sh
@@ -0,0 +1,34 @@
+#MODEL_NAME=ViT-L/14
+#MODEL_NAME=RN50
+MODEL_NAME=ViT-B/16
+DATASET_NAME=fashion
+python cli_vanilla_zeroshot_hf.py fit \
+    --data.num_tasks 1 \
+    --data.current_task 0 \
+    --data.max_length 77 \
+    --data.batch_size 256 \
+    --data.batch_size_zs 32 \
+    --data.num_workers 8 \
+    --data.config config.yaml \
+    --data.dataset_name ${DATASET_NAME} \
+    --data.root_dir /home/ma-user/work/wenzhuoliu/torch_ds \
+    --model.model_name ${MODEL_NAME} \
+    --model.projection_dims 1024 \
+    --model.temperature 0.1 \
+    --model.visual_lr 1e-5 \
+    --model.text_lr 1e-5 \
+    --model.lr_warmup_epochs 5 \
+    --model.weight_decay 0.1 \
+    --model.download_root ./ \
+    --trainer.accelerator npu \
+    --trainer.precision 16 \
+    --trainer.max_epochs 40 \
+    --trainer.log_every_n_steps 1 \
+    --trainer.logger WandbLogger \
+    --trainer.logger.project CLIP-hf \
+    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-vanilla \
+    --trainer.logger.log_model False \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename  ${DATASET_NAME}-${MODEL_NAME}-vanilla
