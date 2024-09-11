24973ed8dd043130321ae538b688ba8011fed70e
lwz <905328134@qq.com>
2024-09-11 17:11:59 +0800

    update to lightning 2.0

--- debug_huggingface.py
+++ debug_huggingface.py
@@ -16,13 +16,22 @@ os.environ['CURL_CA_BUNDLE'] = ''
 # ds = load_dataset("AterMors/wikiart_recaption")
 ds = load_dataset("Norod78/microsoft-fluentui-emoji-512-whitebg")
 
+from transformers import AutoModel, AutoTokenizer
+import ssl
+import os
 
+# Disable SSL verification globally
+ssl._create_default_https_context = ssl._create_unverified_context
 
-ds = load_dataset("Norod78/microsoft-fluentui-emoji-512-whitebg",split="train")
-print(ds[0])
-ds = load_dataset("AterMors/wikiart_recaption",split="train")
-print(ds[0])
-ds = load_dataset("OpenFace-CQUPT/FaceCaption-15M",split="train")
-print(ds[0])
-ds = load_dataset("jmhessel/newyorker_caption_contest",split="train")
-print(ds[0])
+# Optionally, set environment variables to disable SSL certificate verification
+os.environ["TRANSFORMERS_VERBOSITY"] = "error"  # To suppress warnings
+ds = load_dataset("Norod78/microsoft-fluentui-emoji-512-whitebg")
+
+# ds = load_dataset("Norod78/microsoft-fluentui-emoji-512-whitebg",split="train")
+# print(ds[0])
+# ds = load_dataset("AterMors/wikiart_recaption",split="train")
+# print(ds[0])
+# ds = load_dataset("OpenFace-CQUPT/FaceCaption-15M",split="train")
+# print(ds[0])
+# ds = load_dataset("jmhessel/newyorker_caption_contest",split="train")
+# print(ds[0])
