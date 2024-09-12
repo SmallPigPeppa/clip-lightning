6b391422dca32f586b7e864bad9478a8df0cc8bb
lwz <905328134@qq.com>
2024-09-13 00:43:37 +0800

    update to lightning 2.0

--- clip_openai/dataloaders/base_hf.py
+++ clip_openai/dataloaders/base_hf.py
@@ -161,7 +161,12 @@ class ImageRetrievalDataset(Dataset):
 
         # Download the image if it doesn't exist locally
         if not os.path.exists(file_path):
-            response = requests.get(url)
+            # response = requests.get(url)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+            }
+
+            response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 with open(file_path, 'wb') as f:
                     f.write(response.content)
