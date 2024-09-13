ae9cedb12094050d2b8bf7a57e64538b9916f1bf
lwz <905328134@qq.com>
2024-09-14 00:59:42 +0800

    initial

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all2.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all2.sh
@@ -7,7 +7,7 @@ export HF_HOME=/ppio_net0/huggingface
 MODEL_NAME=ViT-B/16
 
 # 数据集列表
-DATASETS=("flickr30k" "coco2014" "emoji" "wikiart" "patfig" "pet" "nouns" "shahnegar" "peanuts" "artbench" "simpsons" "lexica" "styles" "kream" "sketch")
+DATASETS=("emoji" "wikiart" "patfig" "pet" "nouns" "shahnegar" "peanuts" "artbench" "simpsons" "lexica" "styles" "kream" "sketch")
 # 其他参数
 CONFIG_FILE=config.yaml
 ROOT_DIR=/ppio_net0/torch_ds
