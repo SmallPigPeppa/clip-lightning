74d2f55d218fa9f432ee17a3a714e9c1403ae949
lwz <905328134@qq.com>
2024-09-13 16:59:01 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_yd.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_yd.sh
@@ -11,7 +11,7 @@ MODEL_NAME=ViT-B/16
 DATASETS=("emoji")
 # 其他参数
 CONFIG_FILE=config.yaml
-ROOT_DIR=home/ma-user/work/wenzhuoliu/torch_ds
+ROOT_DIR=/home/ma-user/work/wenzhuoliu/torch_ds
 
 # 遍历每个数据集
 for DATASET_NAME in "${DATASETS[@]}"; do
