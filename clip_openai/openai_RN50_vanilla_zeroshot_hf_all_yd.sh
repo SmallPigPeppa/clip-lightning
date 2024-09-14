ebe73bf531ac02ab20e01a63e1d005a7bf9ae6ea
lwz <905328134@qq.com>
2024-09-14 17:52:23 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_yd.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_yd.sh
@@ -8,7 +8,8 @@ MODEL_NAME=ViT-B/16
 
 # 数据集列表
 #DATASETS=("emoji" "wikiart" "patfig" "fashion" "pet" "nouns" "shahnegar" "peanuts" "artbench" "hausavg" "simpsons" "lexica" "styles" "plans" "tomato" "kream" "sketch")
-DATASETS=("nouns" "shahnegar" "peanuts" "artbench" "hausavg" "simpsons" "lexica" "styles" "kream" "sketch")
+#DATASETS=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "artbench" "simpsons" "lexica" "styles"  "kream" "sketch")
+DATASETS=("wikiart" "patfig" "pet" "artbench" "simpsons" "lexica" "styles"  "kream" "sketch")
 # 其他参数
 CONFIG_FILE=config.yaml
 ROOT_DIR=/home/ma-user/work/wenzhuoliu/torch_ds
