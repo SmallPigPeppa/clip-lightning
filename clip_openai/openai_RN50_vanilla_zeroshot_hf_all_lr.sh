e03b0aceb0c3f8960078291b80b5ba5a846f186e
lwz <905328134@qq.com>
2024-09-14 09:15:47 +0800

    initial

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr.sh
@@ -10,7 +10,8 @@ MODEL_NAME=ViT-B/16
 #DATASETS=("emoji" "wikiart" "patfig" "fashion" "pet" "nouns" "shahnegar" "peanuts" "artbench" "hausavg" "simpsons" "lexica" "styles" "plans" "tomato" "kream" "sketch")
 #DATASETS=("styles" "kream")
 #DATASETS=("flickr30k" "coco2014")
-DATASETS=("simpsons")
+#DATASETS=("simpsons")
+DATASETS=("flickr30k" "coco2014" "emoji" "wikiart" "patfig" "pet" "nouns" "shahnegar" "peanuts" "artbench" "simpsons" "lexica" "styles" "kream" "sketch")
 # 其他参数
 CONFIG_FILE=config.yaml
 ROOT_DIR=/ppio_net0/torch_ds
