a21271d962d5324fa74b26a650b42f2550a68f8f
lwz <905328134@qq.com>
2024-09-16 21:27:09 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step.sh
@@ -163,4 +163,4 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   echo "Completed training for dataset: ${DATASET_NAME}"
 done
 
-#/ppio_net0/code/openapi.sh stop a34e5129e26f3e70
+/ppio_net0/code/openapi.sh stop 5107be1343913d61
