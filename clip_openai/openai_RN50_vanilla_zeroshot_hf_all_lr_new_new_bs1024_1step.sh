a709303573dacb70e888512367ada9f361bac769
lwz <905328134@qq.com>
2024-09-16 21:31:10 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step.sh
@@ -35,68 +35,6 @@ for DATASET_NAME in "${DATASETS[@]}"; do
 
   echo "Running training for dataset: ${DATASET_NAME} with lr: ${LR}"
 
-  python cli_vanilla_zeroshot_hf.py fit \
-    --data.num_tasks 1 \
-    --data.current_task 0 \
-    --data.max_length 77 \
-    --data.batch_size 128 \
-    --data.batch_size_zs 32 \
-    --data.num_workers 8 \
-    --data.config ${CONFIG_FILE} \
-    --data.dataset_name ${DATASET_NAME} \
-    --data.root_dir ${ROOT_DIR} \
-    --model.model_name ${MODEL_NAME} \
-    --model.projection_dims 512 \
-    --model.temperature 0.1 \
-    --model.lr ${LR} \
-    --model.lr_warmup_epochs 5 \
-    --model.weight_decay 0.1 \
-    --model.download_root ./ \
-    --model.zero_shot_eval_interval 30 \
-    --trainer.accelerator gpu \
-    --trainer.precision 16 \
-    --trainer.max_epochs 30 \
-    --trainer.log_every_n_steps 1 \
-    --trainer.logger WandbLogger \
-    --trainer.logger.project CLIP-1step \
-    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-vanilla \
-    --trainer.logger.log_model False \
-    --lr_monitor.logging_interval epoch \
-    --model_checkpoint.dirpath ckpt \
-    --model_checkpoint.save_weights_only True \
-    --model_checkpoint.filename ${DATASET_NAME}-${MODEL_NAME}-vanilla
-
-  python cli_distill_zeroshot.py fit \
-    --data.num_tasks 1 \
-    --data.current_task 0 \
-    --data.max_length 77 \
-    --data.batch_size 128 \
-    --data.batch_size_zs 32 \
-    --data.num_workers 8 \
-    --data.config ${CONFIG_FILE} \
-    --data.dataset_name ${DATASET_NAME} \
-    --data.root_dir ${ROOT_DIR} \
-    --model.model_name ${MODEL_NAME} \
-    --model.projection_dims 512 \
-    --model.temperature 0.1 \
-    --model.lr ${LR} \
-    --model.lr_warmup_epochs 5 \
-    --model.weight_decay 0.1 \
-    --model.download_root ./ \
-    --model.zero_shot_eval_interval 30 \
-    --trainer.accelerator gpu \
-    --trainer.precision 16 \
-    --trainer.max_epochs 30 \
-    --trainer.log_every_n_steps 1 \
-    --trainer.logger WandbLogger \
-    --trainer.logger.project CLIP-1step \
-    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-distill \
-    --trainer.logger.log_model False \
-    --lr_monitor.logging_interval epoch \
-    --model_checkpoint.dirpath ckpt \
-    --model_checkpoint.save_weights_only True \
-    --model_checkpoint.filename ${DATASET_NAME}-${MODEL_NAME}-distill
-
   python cli_vanilla_zeroshot_lora fit \
     --data.num_tasks 1 \
     --data.current_task 0 \
@@ -114,10 +52,10 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model.lr_warmup_epochs 5 \
     --model.weight_decay 0.1 \
     --model.download_root ./ \
-    --model.zero_shot_eval_interval 30 \
+    --model.zero_shot_eval_interval 40 \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
-    --trainer.max_epochs 30 \
+    --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger \
     --trainer.logger.project CLIP-1step \
@@ -146,10 +84,10 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model.lr_warmup_epochs 5 \
     --model.weight_decay 0.1 \
     --model.download_root ./ \
-    --model.zero_shot_eval_interval 30 \
+    --model.zero_shot_eval_interval 40 \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
-    --trainer.max_epochs 30 \
+    --trainer.max_epochs 40 \
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger \
     --trainer.logger.project CLIP-1step \
@@ -159,6 +97,68 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
     --model_checkpoint.filename ${DATASET_NAME}-${MODEL_NAME}-dislora
+    
+  python cli_vanilla_zeroshot_hf.py fit \
+    --data.num_tasks 1 \
+    --data.current_task 0 \
+    --data.max_length 77 \
+    --data.batch_size 128 \
+    --data.batch_size_zs 32 \
+    --data.num_workers 8 \
+    --data.config ${CONFIG_FILE} \
+    --data.dataset_name ${DATASET_NAME} \
+    --data.root_dir ${ROOT_DIR} \
+    --model.model_name ${MODEL_NAME} \
+    --model.projection_dims 512 \
+    --model.temperature 0.1 \
+    --model.lr ${LR} \
+    --model.lr_warmup_epochs 5 \
+    --model.weight_decay 0.1 \
+    --model.download_root ./ \
+    --model.zero_shot_eval_interval 40 \
+    --trainer.accelerator gpu \
+    --trainer.precision 16 \
+    --trainer.max_epochs 40 \
+    --trainer.log_every_n_steps 1 \
+    --trainer.logger WandbLogger \
+    --trainer.logger.project CLIP-1step \
+    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-vanilla \
+    --trainer.logger.log_model False \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename ${DATASET_NAME}-${MODEL_NAME}-vanilla
+
+  python cli_distill_zeroshot.py fit \
+    --data.num_tasks 1 \
+    --data.current_task 0 \
+    --data.max_length 77 \
+    --data.batch_size 128 \
+    --data.batch_size_zs 32 \
+    --data.num_workers 8 \
+    --data.config ${CONFIG_FILE} \
+    --data.dataset_name ${DATASET_NAME} \
+    --data.root_dir ${ROOT_DIR} \
+    --model.model_name ${MODEL_NAME} \
+    --model.projection_dims 512 \
+    --model.temperature 0.1 \
+    --model.lr ${LR} \
+    --model.lr_warmup_epochs 5 \
+    --model.weight_decay 0.1 \
+    --model.download_root ./ \
+    --model.zero_shot_eval_interval 40 \
+    --trainer.accelerator gpu \
+    --trainer.precision 16 \
+    --trainer.max_epochs 40 \
+    --trainer.log_every_n_steps 1 \
+    --trainer.logger WandbLogger \
+    --trainer.logger.project CLIP-1step \
+    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-distill \
+    --trainer.logger.log_model False \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename ${DATASET_NAME}-${MODEL_NAME}-distill
 
   echo "Completed training for dataset: ${DATASET_NAME}"
 done
