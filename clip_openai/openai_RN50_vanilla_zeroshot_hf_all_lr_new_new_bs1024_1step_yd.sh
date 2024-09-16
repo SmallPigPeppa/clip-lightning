a689561e4cc3eb1eac1635fc0030bbf720559a0c
lwz <905328134@qq.com>
2024-09-16 22:03:10 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step_yd.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step_yd.sh
@@ -17,16 +17,16 @@ ROOT_DIR=/home/ma-user/work/wenzhuoliu/torch_ds
 
 # 定义不同数据集的学习率
 declare -A DATASET_LR_MAP=(
-  ["flickr30k"]=1e-4
-  ["coco2014"]=1e-4
-  ["wikiart"]=1e-4
-  ["patfig"]=1e-4
-  ["pet"]=1e-4
-  ["simpsons"]=1e-4
-  ["lexica"]=1e-4
-  ["styles"]=1e-4
-  ["kream"]=1e-4
-  ["sketch"]=1e-4
+  ["flickr30k"]=3e-5
+  ["coco2014"]=3e-5
+  ["wikiart"]=3e-5
+  ["patfig"]=3e-5
+  ["pet"]=3e-5
+  ["simpsons"]=3e-5
+  ["lexica"]=3e-5
+  ["styles"]=3e-5
+  ["kream"]=3e-5
+  ["sketch"]=3e-5
 )
 
 # 遍历每个数据集
