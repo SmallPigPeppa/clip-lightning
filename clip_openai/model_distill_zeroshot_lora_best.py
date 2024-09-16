d8bb7b00ca27a776709c185a0d64a87afa9c774f
lwz <905328134@qq.com>
2024-09-17 00:18:07 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_lora_best.py
+++ clip_openai/model_distill_zeroshot_lora_best.py
@@ -34,8 +34,8 @@ def get_lora_model(model):
     # Initialize LoRA configuration with target modules
     lora_config = LoraConfig(
         inference_mode=False,
-        r=16,  # Rank of the low-rank decomposition
-        lora_alpha=32,  # Scaling factor
+        r=4,  # Rank of the low-rank decomposition
+        lora_alpha=8,  # Scaling factor
         task_type=TaskType.SEQ_CLS,  # Task type
         lora_dropout=0.1,  # Dropout rate for LoRA
         target_modules=target_modules  # Specify the target modules
@@ -72,8 +72,8 @@ def get_lora_model_text(model):
     # Initialize LoRA configuration with target modules
     lora_config = LoraConfig(
         inference_mode=False,
-        r=16,  # Rank of the low-rank decomposition
-        lora_alpha=32,  # Scaling factor
+        r=4,  # Rank of the low-rank decomposition
+        lora_alpha=8,  # Scaling factor
         task_type='text',  # Task type
         lora_dropout=0.1,  # Dropout rate for LoRA
         target_modules=target_modules  # Specify the target modules
