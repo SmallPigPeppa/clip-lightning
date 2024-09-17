fe18266a79cbb96d265e678bb256dd2b676bf18e
lwz <905328134@qq.com>
2024-09-17 22:47:26 +0800

    update to lightning 2.0

--- clip_openai/model_vanilla_zeroshot_lora_best.py
+++ clip_openai/model_vanilla_zeroshot_lora_best.py
@@ -189,16 +189,9 @@ class CLIPDualEncoderModel(LightningModule):
         text_embeddings = self.all_gather(text_embeddings)
 
 
-        # self.val_img_feats.append(image_embeddings)
-        # self.val_text_feats.append(text_embeddings)
-        # 将 all_gather 后的数据调整为合适的维度
-        image_embeddings = image_embeddings.view(-1, image_embeddings.size(-1))  # (num_gpus * batch_size, feature_dim)
-        text_embeddings = text_embeddings.view(-1, text_embeddings.size(-1))  # (num_gpus * batch_size, feature_dim)
+        self.val_img_feats.append(image_embeddings)
+        self.val_text_feats.append(text_embeddings)
 
-        # 仅在主进程上保存特征
-        if self.trainer.is_global_zero:
-            self.val_img_feats.append(image_embeddings)
-            self.val_text_feats.append(text_embeddings)
 
         return clip_loss
 
@@ -211,6 +204,7 @@ class CLIPDualEncoderModel(LightningModule):
     def on_validation_epoch_end(self):
         all_image_features = torch.cat(self.val_img_feats)
         all_text_features = torch.cat(self.val_text_feats)
+        print(all_image_features)
         val_metrics = self.get_clip_metrics_cpu(
             image_features=all_image_features,
             text_features=all_text_features,
