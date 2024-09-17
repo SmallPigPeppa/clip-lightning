e79addbaa551c94b4262fb4f9e2bec1d42a8eb63
lwz <905328134@qq.com>
2024-09-17 15:13:43 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_lora_best.py
+++ clip_openai/model_distill_zeroshot_lora_best.py
@@ -247,12 +247,12 @@ class CLIPDualEncoderModel(LightningModule):
         if self.distill:
             parameters.append({
                 "params": self.distill_predictor1.parameters(),
-                "lr": self.hparams.lr,  # 可以根据需要调整学习率
+                "lr": self.hparams.lr * 2.,  # 可以根据需要调整学习率
                 "weight_decay": self.hparams.weight_decay
             })
             parameters.append({
                 "params": self.distill_predictor2.parameters(),
-                "lr": self.hparams.lr,  # 可以根据需要调整学习率
+                "lr": self.hparams.lr * 2.,  # 可以根据需要调整学习率
                 "weight_decay": self.hparams.weight_decay
             })
         else:
@@ -468,11 +468,11 @@ class CLIPDualEncoderModel(LightningModule):
         del self.zero_shot_classifier
         return metrics
 
-    def on_before_optimizer_step(self, optimizer) -> None:
-        print("**************on_before_opt enter1*********")
-        for name, param in self.model.named_parameters():
-            if param.grad is not None:
-                print(name)
-            # if param.requires_grad :
-            #     print(name)
-        print("***************on_before_opt exit1*********")
+    # def on_before_optimizer_step(self, optimizer) -> None:
+    #     print("**************on_before_opt enter1*********")
+    #     for name, param in self.model.named_parameters():
+    #         if param.grad is not None:
+    #             print(name)
+    #         # if param.requires_grad :
+    #         #     print(name)
+    #     print("***************on_before_opt exit1*********")
