2136a9b885742585e0fb6c495f36c8c86df9660f
lwz <905328134@qq.com>
2024-09-17 19:06:00 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot.py
+++ clip_openai/model_distill_zeroshot.py
@@ -120,8 +120,8 @@ class CLIPDualEncoderModel(LightningModule):
             optimizer,
             warmup_epochs=self.hparams.lr_warmup_epochs,
             max_epochs=self.trainer.max_epochs,
-            warmup_start_lr=0.01 * self.hparams.lr,
-            eta_min=0.01 * self.hparams.lr
+            warmup_start_lr=0.001 * self.hparams.lr,
+            eta_min=0.001 * self.hparams.lr
         )
 
         return {
