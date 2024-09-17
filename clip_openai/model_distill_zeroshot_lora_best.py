99dda740c550d520acc0293e40f8e19b21ffe6e3
lwz <905328134@qq.com>
2024-09-17 14:42:17 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_lora_best.py
+++ clip_openai/model_distill_zeroshot_lora_best.py
@@ -52,8 +52,8 @@ def get_lora_model_vision(model):
     target_modules = find_target_modules(model)
     lora_config = LoraConfig(
         inference_mode=False,
-        r=8,  # Rank of the low-rank decomposition
-        lora_alpha=16,  # Scaling factor
+        r=16,  # Rank of the low-rank decomposition
+        lora_alpha=32,  # Scaling factor
         task_type='vision',  # Task type
         lora_dropout=0.1,  # Dropout rate for LoRA
         target_modules=target_modules,
@@ -71,8 +71,8 @@ def get_lora_model_text(model):
     # Initialize LoRA configuration with target modules
     lora_config = LoraConfig(
         inference_mode=False,
-        r=8,  # Rank of the low-rank decomposition
-        lora_alpha=16,  # Scaling factor
+        r=16,  # Rank of the low-rank decomposition
+        lora_alpha=32,  # Scaling factor
         task_type='text',  # Task type
         lora_dropout=0.1,  # Dropout rate for LoRA
         target_modules=target_modules  # Specify the target modules
@@ -145,8 +145,8 @@ class CLIPDualEncoderModel(LightningModule):
         conv1 = NewModel(copy.deepcopy(self.model.visual.conv1))
         lora_config = LoraConfig(
             inference_mode=False,
-            r=8,  # Rank of the low-rank decomposition
-            lora_alpha=16,  # Scaling factor
+            r=16,  # Rank of the low-rank decomposition
+            lora_alpha=32,  # Scaling factor
             lora_dropout=0.1,  # Dropout rate for LoRA
             target_modules=['conv1'],
         )
