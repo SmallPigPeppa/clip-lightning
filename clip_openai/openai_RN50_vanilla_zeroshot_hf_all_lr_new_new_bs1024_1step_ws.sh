0bd50f2d2a5d2c860f507fb566daceb5f7390a37
lwz <905328134@qq.com>
2024-09-18 05:16:18 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step_ws.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step_ws.sh
@@ -3,6 +3,7 @@
 # 设置 Hugging Face home 目录
 export HF_HOME=/home/ma-user/work/dataset/all/hf-datasets
 export HF_ENDPOINT=https://hf-mirror.com
+export CURL_CA_BUNDLE=''
 
 # 模型名称
 MODEL_NAME=ViT-B/16
