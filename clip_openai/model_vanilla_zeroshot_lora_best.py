d4d257db741a19c75db37b18ea9d1265b2c853f6
lwz <905328134@qq.com>
2024-09-17 21:27:33 +0800

    update to lightning 2.0

--- clip_openai/model_vanilla_zeroshot_lora_best.py
+++ clip_openai/model_vanilla_zeroshot_lora_best.py
@@ -116,13 +116,13 @@ class CLIPDualEncoderModel(LightningModule):
         # load task N-1 checkpoint
         if self.hparams.old_checkpoint_path is not None:
             checkpoint = torch.load(self.hparams.old_checkpoint_path, map_location=torch.device('cpu'))
-            import pdb;pdb.set_trace()
-            filtered_state_dict = {k: v for k, v in checkpoint['state_dict'].items() if not k.startswith(
-                (
-                    'model_old',
-                    'distill_predictor'
-                ))}
-            self.model.load_state_dict(filtered_state_dict, strict=True)
+            # import pdb;pdb.set_trace()
+            # filtered_state_dict = {k: v for k, v in checkpoint['model'].items() if not k.startswith(
+            #     (
+            #         'model_old',
+            #         'distill_predictor'
+            #     ))}
+            self.model.load_state_dict(checkpoint['model'], strict=True)
             print("Model weights loaded successfully and old parts copied.")
 
         self.model_old = copy.deepcopy(self.model)
