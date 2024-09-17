35bf28f57db4c8b8cda49b70a7b18521bcc019bf
lwz <905328134@qq.com>
2024-09-17 23:34:15 +0800

    update to lightning 2.0

--- clip_openai/model_vanilla_zeroshot_lora_best.py
+++ clip_openai/model_vanilla_zeroshot_lora_best.py
@@ -253,8 +253,8 @@ class CLIPDualEncoderModel(LightningModule):
             preds = torch.where(ranking == ground_truth)[1]
             preds = preds.detach().cpu().numpy()
             for k in [1]:
-                # metrics[f"{name}_R@{k}"] = np.mean(preds < k) * 100  # Convert recall to percentage
-                metrics[f"{name}_R@{k}"] = torch.tensor(np.mean(preds < k) * 100).to(self.device)
+                metrics[f"{name}_R@{k}"] = np.mean(preds < k) * 100  # Convert recall to percentage
+                # metrics[f"{name}_R@{k}"] = torch.tensor(np.mean(preds < k) * 100).to(self.device)
 
         return metrics
 
