d64dcfa8f4191fada2291fab7b978e96e482f1a9
lwz <905328134@qq.com>
2024-09-19 03:09:04 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_ws.sh
+++ clip_openai/aaaa_train_ws.sh
@@ -11,6 +11,7 @@ MODEL_NAME=ViT-B/16
 # 数据集列表
 DATASETS=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "styles" "kream" "sketch")
 DATASETS=("flickr30k")
+DATASETS=("coco2014")
 
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
