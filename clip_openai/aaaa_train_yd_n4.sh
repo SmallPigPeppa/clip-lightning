fe2d503a23e09deccbaf3566bf646af1530a0887
lwz <905328134@qq.com>
2024-09-19 03:11:05 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_yd_n4.sh
+++ clip_openai/aaaa_train_yd_n4.sh
@@ -81,7 +81,7 @@ run_training() {
     --lr_monitor.logging_interval epoch \
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
-    --model_checkpoint.filename ${dataset_name}-1024/${method}-lr-${lr}
+    --model_checkpoint.filename ${dataset_name}-1024-ydn4/${method}-lr-${lr}
 }
 
 # 运行所有数据集
