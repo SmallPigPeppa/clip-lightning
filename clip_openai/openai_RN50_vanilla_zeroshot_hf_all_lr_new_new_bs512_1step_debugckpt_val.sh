1a05edbc8f90aa540b8b9cb52928775856de621d
lwz <905328134@qq.com>
2024-09-19 02:23:44 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs512_1step_debugckpt_val.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs512_1step_debugckpt_val.sh
@@ -58,7 +58,7 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model.zero_shot_eval_interval 40 \
     --model.old_checkpoint_path ckpt/${DATASET_NAME}-512/distill-new-v1.ckpt  \
     --trainer.accelerator gpu \
-    --trainer.devices 4 \
+    --trainer.devices 1 \
     --trainer.precision 16 \
     --trainer.max_epochs 1 \
     --trainer.log_every_n_steps 1 \
