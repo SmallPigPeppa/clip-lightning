49748c233f2b4a75074f36c497ef93367f14147a
lwz <905328134@qq.com>
2024-09-19 01:21:20 +0800

    update to lightning 2.0

--- clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step_fixbug_yd.sh
+++ clip_openai/openai_RN50_vanilla_zeroshot_hf_all_lr_new_new_bs1024_1step_fixbug_yd.sh
@@ -10,9 +10,9 @@ MODEL_NAME=ViT-B/16
 # 数据集列表
 DATASETS=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "styles" "kream" "sketch")
 DATASETS=("coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "styles" "kream" "sketch")
-#DATASETS=("flickr30k")
-DATASETS=("coco2014")
-DATASETS=("wikiart")
+DATASETS=("flickr30k")
+#DATASETS=("coco2014")
+#DATASETS=("wikiart")
 
 
 # 其他参数
@@ -41,38 +41,37 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   echo "Running training for dataset: ${DATASET_NAME} with lr: ${LR}"
 
 
-
-#  python cli_distill_zeroshot_lora_best.py fit \
-#    --data.num_tasks 1 \
-#    --data.current_task 0 \
-#    --data.max_length 77 \
-#    --data.batch_size 128 \
-#    --data.batch_size_zs 32 \
-#    --data.num_workers 8 \
-#    --data.config ${CONFIG_FILE} \
-#    --data.dataset_name ${DATASET_NAME} \
-#    --data.root_dir ${ROOT_DIR} \
-#    --model.model_name ${MODEL_NAME} \
-#    --model.projection_dims 512 \
-#    --model.temperature 0.1 \
-#    --model.lr ${LR} \
-#    --model.lr_warmup_epochs 5 \
-#    --model.weight_decay 0.1 \
-#    --model.download_root ./ \
-#    --model.zero_shot_eval_interval 40 \
-#    --trainer.accelerator npu \
-#    --trainer.precision 16 \
-#    --trainer.max_epochs 40 \
-#    --trainer.log_every_n_steps 1 \
-#    --trainer.logger WandbLogger \
-#    --trainer.logger.project CLIP-1step-1024-yd \
-#    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-dislora \
-#    --trainer.logger.log_model False \
-#    --trainer.strategy ddp_find_unused_parameters_true \
-#    --lr_monitor.logging_interval epoch \
-#    --model_checkpoint.dirpath ckpt \
-#    --model_checkpoint.save_weights_only True \
-#    --model_checkpoint.filename ${DATASET_NAME}-1024/dislora
+  python cli_distill_zeroshot_lora_best.py fit \
+    --data.num_tasks 1 \
+    --data.current_task 0 \
+    --data.max_length 77 \
+    --data.batch_size 128 \
+    --data.batch_size_zs 32 \
+    --data.num_workers 8 \
+    --data.config ${CONFIG_FILE} \
+    --data.dataset_name ${DATASET_NAME} \
+    --data.root_dir ${ROOT_DIR} \
+    --model.model_name ${MODEL_NAME} \
+    --model.projection_dims 512 \
+    --model.temperature 0.1 \
+    --model.lr ${LR} \
+    --model.lr_warmup_epochs 5 \
+    --model.weight_decay 0.1 \
+    --model.download_root ./ \
+    --model.zero_shot_eval_interval 40 \
+    --trainer.accelerator npu \
+    --trainer.precision 16 \
+    --trainer.max_epochs 40 \
+    --trainer.log_every_n_steps 1 \
+    --trainer.logger WandbLogger \
+    --trainer.logger.project CLIP-1step-1024-yd \
+    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-dislora \
+    --trainer.logger.log_model False \
+    --trainer.strategy ddp_find_unused_parameters_true \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename ${DATASET_NAME}-1024/dislora
 
   python cli_distill_zeroshot_lora_best_new.py fit \
     --data.num_tasks 1 \
@@ -86,7 +85,7 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --data.root_dir ${ROOT_DIR} \
     --model.model_name ${MODEL_NAME} \
     --model.projection_dims 512 \
-    --model.projection_lr_scale 10 \
+    --model.projection_lr_scale 4 \
     --model.temperature 0.1 \
     --model.lr ${LR} \
     --model.lr_warmup_epochs 5 \
@@ -106,7 +105,7 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
     --model_checkpoint.filename ${DATASET_NAME}-1024/dislora-new
-#
+
 #  python cli_distill_zeroshot.py fit \
 #    --data.num_tasks 1 \
 #    --data.current_task 0 \
@@ -139,38 +138,71 @@ for DATASET_NAME in "${DATASETS[@]}"; do
 #    --model_checkpoint.save_weights_only True \
 #    --model_checkpoint.filename ${DATASET_NAME}-1024/distill
 
+  python cli_distill_zeroshot_lora_best.py fit \
+    --data.num_tasks 1 \
+    --data.current_task 0 \
+    --data.max_length 77 \
+    --data.batch_size 128 \
+    --data.batch_size_zs 32 \
+    --data.num_workers 8 \
+    --data.config ${CONFIG_FILE} \
+    --data.dataset_name ${DATASET_NAME} \
+    --data.root_dir ${ROOT_DIR} \
+    --model.model_name ${MODEL_NAME} \
+    --model.projection_dims 512 \
+    --model.temperature 0.1 \
+    --model.lr ${LR} \
+    --model.lr_warmup_epochs 5 \
+    --model.weight_decay 0.1 \
+    --model.download_root ./ \
+    --model.zero_shot_eval_interval 40 \
+    --trainer.accelerator gpu \
+    --trainer.precision 16 \
+    --trainer.max_epochs 40 \
+    --trainer.log_every_n_steps 1 \
+    --trainer.logger WandbLogger \
+    --trainer.logger.project CLIP-1step-1024 \
+    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-dislora \
+    --trainer.logger.log_model False \
+    --trainer.strategy ddp_find_unused_parameters_true \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename ${DATASET_NAME}/dislora
 
-#  python cli_vanilla_zeroshot_hf.py fit \
-#    --data.num_tasks 1 \
-#    --data.current_task 0 \
-#    --data.max_length 77 \
-#    --data.batch_size 128 \
-#    --data.batch_size_zs 32 \
-#    --data.num_workers 8 \
-#    --data.config ${CONFIG_FILE} \
-#    --data.dataset_name ${DATASET_NAME} \
-#    --data.root_dir ${ROOT_DIR} \
-#    --model.model_name ${MODEL_NAME} \
-#    --model.projection_dims 512 \
-#    --model.temperature 0.1 \
-#    --model.lr ${LR} \
-#    --model.lr_warmup_epochs 5 \
-#    --model.weight_decay 0.1 \
-#    --model.download_root ./ \
-#    --model.zero_shot_eval_interval 40 \
-#    --trainer.accelerator npu \
-#    --trainer.precision 16 \
-#    --trainer.max_epochs 40 \
-#    --trainer.log_every_n_steps 1 \
-#    --trainer.logger WandbLogger \
-#    --trainer.logger.project CLIP-1step-1024-yd \
-#    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-vanilla \
-#    --trainer.logger.log_model False \
-#    --trainer.strategy ddp_find_unused_parameters_true \
-#    --lr_monitor.logging_interval epoch \
-#    --model_checkpoint.dirpath ckpt \
-#    --model_checkpoint.save_weights_only True \
-#    --model_checkpoint.filename ${DATASET_NAME}-1024/vanilla
+
+
+  python cli_vanilla_zeroshot_hf.py fit \
+    --data.num_tasks 1 \
+    --data.current_task 0 \
+    --data.max_length 77 \
+    --data.batch_size 128 \
+    --data.batch_size_zs 32 \
+    --data.num_workers 8 \
+    --data.config ${CONFIG_FILE} \
+    --data.dataset_name ${DATASET_NAME} \
+    --data.root_dir ${ROOT_DIR} \
+    --model.model_name ${MODEL_NAME} \
+    --model.projection_dims 512 \
+    --model.temperature 0.1 \
+    --model.lr ${LR} \
+    --model.lr_warmup_epochs 5 \
+    --model.weight_decay 0.1 \
+    --model.download_root ./ \
+    --model.zero_shot_eval_interval 40 \
+    --trainer.accelerator npu \
+    --trainer.precision 16 \
+    --trainer.max_epochs 40 \
+    --trainer.log_every_n_steps 1 \
+    --trainer.logger WandbLogger \
+    --trainer.logger.project CLIP-1step-1024-yd \
+    --trainer.logger.name ${DATASET_NAME}-${MODEL_NAME}-vanilla \
+    --trainer.logger.log_model False \
+    --trainer.strategy ddp_find_unused_parameters_true \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename ${DATASET_NAME}-1024/vanilla
 
   echo "Completed training for dataset: ${DATASET_NAME}"
 done
