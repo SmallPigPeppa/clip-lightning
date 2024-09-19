c37b8aa40a57c9a25fd63b9ec2c0494f99aff966
lwz <905328134@qq.com>
2024-09-19 18:19:53 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs512_yd.sh
+++ clip_openai/aaaa_train_bs512_yd.sh
@@ -10,8 +10,8 @@ MODEL_NAME=ViT-B/16
 
 # 数据集列表
 DATASETS=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "styles" "kream" "sketch")
-DATASETS=("coco2014")
-DATASETS=("flickr30k" "coco2014")
+#DATASETS=("coco2014")
+#DATASETS=("flickr30k" "coco2014")
 
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
