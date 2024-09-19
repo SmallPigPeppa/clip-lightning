42a3207ca4ff812ddfe391438e9c887a36cda253
lwz <905328134@qq.com>
2024-09-19 17:23:27 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs512.sh
+++ clip_openai/aaaa_train_bs512.sh
@@ -30,7 +30,7 @@ declare -A DATASET_LR_MAP=(
 # 脚本方法映射
 declare -A METHOD_MAP=(
   ["vanilla"]="cli_vanilla_zeroshot_hf.py"
-  ["lora_best"]="cli_vanilla_zeroshot_lora_bestv2.py"
+  ["lora_best"]="cli_vanilla_zeroshot_lora_best.py"
   ["distill"]="cli_distill_zeroshot.py"
   ["distill_lora"]="cli_distill_zeroshot_lora_best.py"
 )
@@ -90,9 +90,9 @@ for DATASET_NAME in "${DATASETS[@]}"; do
 
   # 按不同的方法运行（比如 'distill_lora', 'vanilla'）
   run_training "vanilla" ${DATASET_NAME} ${LR}
-  run_training "lora" ${DATASET_NAME} ${LR}
-  run_training "distill" ${DATASET_NAME} ${LR}
-  run_training "distill_lora" ${DATASET_NAME} ${LR}
+#  run_training "lora" ${DATASET_NAME} ${LR}
+#  run_training "distill" ${DATASET_NAME} ${LR}
+#  run_training "distill_lora" ${DATASET_NAME} ${LR}
 
 
   echo "Completed training for dataset: ${DATASET_NAME}"
--- clip_openai/model_vanilla_zeroshot_lora.py
+++ clip_openai/model_vanilla_zeroshot_lora.py
@@ -150,15 +150,19 @@ class CLIPDualEncoderModel(LightningModule):
 
     def on_train_start(self):
         # recall metric
+        self.model.eval()
         val_loader = self.trainer.datamodule.val_dataloader()
         recall_metric = self.get_recall_metrics(val_loader)
         self.log_dict(recall_metric, sync_dist=True)
+        self.model.train()
+
         # # Zero-shot metric evaluation before training starts
         # zero_shot_loader = self.trainer.datamodule.zero_shot_dataloader()
         # zero_shot_metric = self.get_zero_shot_metrics(zero_shot_loader)
         # self.log_dict(zero_shot_metric, sync_dist=True)
 
     def on_validation_epoch_end(self):
+        self.model.eval()
         # recall metric
         if (self.current_epoch + 1) % self.hparams.recall_eval_interval == 0:
             val_loader = self.trainer.datamodule.val_dataloader()
