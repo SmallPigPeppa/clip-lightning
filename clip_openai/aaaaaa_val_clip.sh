10969a200bc847229ecdffbe112aa8286ee50703
lwz <905328134@qq.com>
2024-09-19 17:38:00 +0800

    update to lightning 2.0

--- clip_openai/aaaaaa_val_clip.sh
+++ clip_openai/aaaaaa_val_clip.sh
@@ -56,7 +56,7 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model.weight_decay 0.1 \
     --model.download_root ./ \
     --model.recall_eval_interval 1 \
-    --model.zero_shot_eval_interval 1 \
+    --model.zero_shot_eval_interval 40 \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
     --trainer.max_epochs 1 \
