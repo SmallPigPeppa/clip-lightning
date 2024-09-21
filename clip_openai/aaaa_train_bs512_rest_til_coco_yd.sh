217842b442608ca034e7ea87891ef73914e1f761
lwz <905328134@qq.com>
2024-09-21 23:53:41 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs512_rest_til_coco_yd.sh
+++ clip_openai/aaaa_train_bs512_rest_til_coco_yd.sh
@@ -21,7 +21,7 @@ DATASETS=("coco2014")
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
   ["flickr30k"]=2e-5
-  ["coco2014"]=2e-6
+  ["coco2014"]=1e-5
   ["lexica"]=2e-5
   ["pet"]=5e-6
   ["wikiart"]=2e-5
