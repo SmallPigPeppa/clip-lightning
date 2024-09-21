fe739dbaff12a904def6742d44185bfabecc7933
lwz <905328134@qq.com>
2024-09-21 22:19:59 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs512_rest_til_wiki.sh
+++ clip_openai/aaaa_train_bs512_rest_til_wiki.sh
@@ -83,7 +83,7 @@ run_training() {
     --model.weight_decay 0.1 \
     --model.download_root ./ \
     --model.zero_shot_eval_interval ${ZERO_SHOT_EVAL_INTERVAL} \
-    --model.old_checkpoint_path ckpt-til/lexica-512/$lora-lr-7.5e-6.ckpt  \
+    --model.old_checkpoint_path ckpt-til/lexica-512/lora-lr-7.5e-6.ckpt  \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
     --trainer.max_epochs ${MAX_EPOCHS} \
