c7026d93d9ae0a2210bedaac45ea9f95232ec835
lwz <905328134@qq.com>
2024-09-22 22:36:32 +0800

    update to lightning 2.0

--- clip_openai/model_distill_zeroshot_zscl.py
+++ clip_openai/model_distill_zeroshot_zscl.py
@@ -319,7 +319,7 @@ class CLIPDualEncoderModel(LightningModule):
             )
 
             self.log("train/distill_loss", distill_loss, sync_dist=True)
-            return clip_loss + distill_loss * 0.1
+            return clip_loss + distill_loss * 0.2
         else:
             return clip_loss
 
