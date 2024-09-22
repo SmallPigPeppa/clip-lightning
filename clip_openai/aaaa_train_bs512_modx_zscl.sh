0298f53410959cb145a4d3d00b6c2b78da9dfdb8
lwz <905328134@qq.com>
2024-09-22 20:44:48 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs512_modx_zscl.sh
+++ clip_openai/aaaa_train_bs512_modx_zscl.sh
@@ -24,7 +24,7 @@ DATASETS=("flickr30k")
 
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
-  ["flickr30k"]=4e-5
+  ["flickr30k"]=2e-5
   ["coco2014"]=2e-5
   ["wikiart"]=2e-5
   ["patfig"]=2e-5
@@ -115,8 +115,8 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   # 按不同的方法运行（比如 'distill_lora', 'vanilla'）
 #  run_training "vanilla" ${DATASET_NAME} ${LR}
 #  run_training "distill" ${DATASET_NAME} ${LR}
-  run_training "modx" ${DATASET_NAME} ${LR}
-#  run_training "zscl" ${DATASET_NAME} ${LR}
+#  run_training "modx" ${DATASET_NAME} ${LR}
+  run_training "zscl" ${DATASET_NAME} ${LR}
 #  run_training "lora" ${DATASET_NAME} ${LR}
 #  run_training "distill_lora" ${DATASET_NAME} ${LR}
 
