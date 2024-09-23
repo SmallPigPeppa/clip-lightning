c4179e2b1a730aab6b0bd2546df057f9c081633b
lwz <905328134@qq.com>
2024-09-24 00:00:21 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_cl.sh
+++ clip_openai/aaa_val_cl.sh
@@ -7,9 +7,9 @@ MODEL_NAME=ViT-B/16
 
 # 数据集列表
 DATASETS=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "styles" "kream" "sketch")
-#DATASETS=("flickr30k")
+DATASETS=("flickr30k")
 #DATASETS=("wikiart")
-DATASETS=("coco2014")
+#DATASETS=("coco2014")
 #DATASETS=("wikiart")
 #DATASETS=("kream")
 
@@ -60,7 +60,7 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model.download_root ./ \
     --model.recall_eval_interval 1 \
     --model.zero_shot_eval_interval 40 \
-    --model.old_checkpoint_path ckpt-til/lexica-512/lora-lr-7.5e-6.ckpt  \
+    --model.old_checkpoint_path ckpt-til/lexica-512-new/vanilla-lr-7.5e-6.ckpt  \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
     --trainer.max_epochs 1 \
