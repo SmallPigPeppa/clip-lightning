8075d2661196a9d7d5603b1d9903e3a350ab5d28
lwz <905328134@qq.com>
2024-09-24 23:06:51 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_flickr.sh
+++ clip_openai/aaaa_train_bs1024_flickr.sh
@@ -126,11 +126,11 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   echo "Running training for dataset: ${DATASET_NAME} with lr: ${LR}"
 
   # 按不同的方法运行（比如 'distill_lora', 'vanilla'）
-  run_training "vanilla" ${DATASET_NAME} ${LR} ${LR_TEXT}
-#  run_training "distill" ${DATASET_NAME} ${LR} ${LR_TEXT}
-#  run_training "lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
-#  run_training "lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
-#  run_training "distill_lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
+#  run_training "vanilla" ${DATASET_NAME} ${LR} ${LR_TEXT}
+  run_training "distill" ${DATASET_NAME} ${LR} ${LR_TEXT}
+  run_training "lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
+  run_training "lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
+  run_training "distill_lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
 #  run_training "distill_lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
 
 
