ebcf1f20a01b9c77318fc9d4b0fe99528f015125
lwz <905328134@qq.com>
2024-09-25 00:11:31 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_flickr.sh
+++ clip_openai/aaaa_train_bs1024_flickr.sh
@@ -15,7 +15,7 @@ DATASETS=("flickr30k")
 
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
-  ["flickr30k"]=8.6e-6
+  ["flickr30k"]=8e-6
   ["coco2014"]=1e-5
   ["wikiart"]=1e-5
   ["patfig"]=1e-5
@@ -36,7 +36,7 @@ declare -A DATASET_LR_MAP=(
 )
 
 declare -A DATASET_LR_TEXT_MAP=(
-  ["flickr30k"]=1.3e-4
+  ["flickr30k"]=1.2e-4
   ["coco2014"]=2e-4
   ["wikiart"]=2e-4
   ["patfig"]=2e-4
@@ -127,11 +127,11 @@ for DATASET_NAME in "${DATASETS[@]}"; do
 
   # 按不同的方法运行（比如 'distill_lora', 'vanilla'）
 #  run_training "vanilla" ${DATASET_NAME} ${LR} ${LR_TEXT}
-  run_training "distill" ${DATASET_NAME} ${LR} ${LR_TEXT}
-  run_training "lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
-  run_training "lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
-  run_training "distill_lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
-#  run_training "distill_lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
+#  run_training "distill" ${DATASET_NAME} ${LR} ${LR_TEXT}
+#  run_training "lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
+#  run_training "lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
+#  run_training "distill_lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
+  run_training "distill_lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
 
 
   echo "Completed training for dataset: ${DATASET_NAME}"
