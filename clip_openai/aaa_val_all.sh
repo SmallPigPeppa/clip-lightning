89adcf479674193fff07dc0dfdf69b3241a823e0
lwz <905328134@qq.com>
2024-09-25 15:50:44 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_all.sh
+++ clip_openai/aaa_val_all.sh
@@ -23,7 +23,7 @@ ROOT_DIR=/ppio_net0/torch_ds
 
 # 评估所有数据集
 python cli_val.py validate \
-    --data.dataset_names ${DATASETS} \
+    --data.dataset_name ${DATASETS} \
     --data.num_tasks 1 \
     --data.current_task 0 \
     --data.max_length 77 \
