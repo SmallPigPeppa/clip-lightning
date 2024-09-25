f907ca05fc57b46ac0a06a01a3a3f367f4557c97
lwz <905328134@qq.com>
2024-09-26 00:35:52 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_lexica.sh
+++ clip_openai/aaaa_train_bs1024_lexica.sh
@@ -20,6 +20,7 @@ DATASETS=("lexica")
 CKPTS=(
     "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
     "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
+    "ckpt/pet-1024/distill_lora_v2-lr-2e-5-lr_text-4e-5.ckpt"
 )
 
 # 拼接检查点
