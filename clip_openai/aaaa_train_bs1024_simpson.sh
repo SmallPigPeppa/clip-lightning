8f768fbdd11b7984e0c8d5de666445460d7e03e5
lwz <905328134@qq.com>
2024-09-26 03:35:08 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_simpson.sh
+++ clip_openai/aaaa_train_bs1024_simpson.sh
@@ -31,7 +31,7 @@ CKPTS_COMMA_JOINED=$(IFS=','; echo "${CKPTS[*]}")
 
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
-  ["simpsons"]=3e-5
+  ["simpsons"]=8e-5
   ["lexica"]=3e-5
   ["pet"]=3e-5
   ["flickr30k"]=8e-6
@@ -53,7 +53,7 @@ declare -A DATASET_LR_MAP=(
 
 
 declare -A DATASET_LR_TEXT_MAP=(
-  ["simpsons"]=1e-4
+  ["simpsons"]=8e-5
   ["lexica"]=3e-5
   ["pet"]=6e-5
   ["flickr30k"]=1.2e-4
