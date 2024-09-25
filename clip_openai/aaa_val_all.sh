e0445ec4d24b77b24e3b73e11eeff62710301f25
lwz <905328134@qq.com>
2024-09-25 22:55:48 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_all.sh
+++ clip_openai/aaa_val_all.sh
@@ -53,7 +53,7 @@ python cli_val.py validate \
     --data.num_workers 8 \
     --data.config ${CONFIG_FILE} \
     --data.root_dir ${ROOT_DIR} \
-    --model.evaluate_zero_shot False \
+    --model.evaluate_zero_shot True \
     --model.result_path metrics_evaluation.xlsx \
     --model.model_name ${MODEL_NAME} \
     --model.download_root ./ \
