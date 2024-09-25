2283311eeb26a2c5603c69ccdcd89d6f6ec44bb4
lwz <905328134@qq.com>
2024-09-25 21:51:08 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_lexica.sh
+++ clip_openai/aaaa_train_bs1024_lexica.sh
@@ -41,7 +41,7 @@ declare -A DATASET_LR_MAP=(
 
 declare -A DATASET_LR_TEXT_MAP=(
   ["pet"]=3e-5
-  ["lexica"]=5e-4
+  ["lexica"]=3e-5
   ["flickr30k"]=1.2e-4
   ["coco2014"]=2e-4
   ["wikiart"]=2e-4
@@ -131,11 +131,11 @@ for DATASET_NAME in "${DATASETS[@]}"; do
 
   # 按不同的方法运行（比如 'distill_lora', 'vanilla'）
 #  run_training "vanilla" ${DATASET_NAME} ${LR} ${LR_TEXT}
-#  run_training "distill" ${DATASET_NAME} ${LR} ${LR_TEXT}
+  run_training "distill" ${DATASET_NAME} ${LR} ${LR_TEXT}
 #  run_training "lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
 #  run_training "lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
 #  run_training "distill_lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
-  run_training "distill_lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
+#  run_training "distill_lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
 
 
   echo "Completed training for dataset: ${DATASET_NAME}"
