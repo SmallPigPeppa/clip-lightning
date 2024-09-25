758628328c59396a964e6fdeff7f774ac32b93cc
lwz <905328134@qq.com>
2024-09-26 04:06:03 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_pet.sh
+++ clip_openai/aaaa_train_bs1024_pet.sh
@@ -26,7 +26,7 @@ CKPTS_COMMA_JOINED=$(IFS=','; echo "${CKPTS[*]}")
 
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
-  ["pet"]=2e-5
+  ["pet"]=5e-4
   ["flickr30k"]=8e-6
   ["coco2014"]=1e-5
   ["wikiart"]=1e-5
@@ -48,7 +48,7 @@ declare -A DATASET_LR_MAP=(
 
 
 declare -A DATASET_LR_TEXT_MAP=(
-  ["pet"]=4e-5
+  ["pet"]=5e-4
   ["flickr30k"]=1.2e-4
   ["coco2014"]=2e-4
   ["wikiart"]=2e-4
@@ -150,5 +150,5 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   echo "Completed training for dataset: ${DATASET_NAME}"
 done
 
-#/ppio_net0/code/openapi.sh stop a2b028e85b48907c
+/ppio_net0/code/openapi.sh stop a2b028e85b48907c
 
