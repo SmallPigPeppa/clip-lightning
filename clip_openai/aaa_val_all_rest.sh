eefe56eb99f52f98f0548d7cc245a16da07ed538
lwz <905328134@qq.com>
2024-09-25 21:08:41 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_all_rest.sh
+++ clip_openai/aaa_val_all_rest.sh
@@ -8,8 +8,8 @@ MODEL_NAME=ViT-B/16
 # 数据集列表，拼接成逗号分隔的字符串
 datasets=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "styles" "kream" "sketch")
 datasets=(
-"flickr30k"
-#"coco2014"
+#"flickr30k"
+"coco2014"
 #"pet"
 #"lexica"
 #"simpsons"
@@ -25,7 +25,8 @@ DATASETS=$(IFS=','; echo "${datasets[*]}")
 # 定义检查点列表
 CKPTS=(
 #  "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
-  "ckpt/flickr30k-1024/vanilla-lr-8.6e-6-lr_text-1.3e-4.ckpt"
+#  "ckpt/flickr30k-1024/vanilla-lr-8.6e-6-lr_text-1.3e-4.ckpt"
+  "ckpt/coco-1024/vanilla-lr-1e-8-lr_text-5e-4.ckpt"
 #  "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
 #  "ckpt/pet-1024/distill_lora_v2-lr-1e-5-lr_text-4e-4.ckpt"
 )
