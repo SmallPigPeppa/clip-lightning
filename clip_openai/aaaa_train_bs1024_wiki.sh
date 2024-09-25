473b456b875dcb3726fecacb7eaff70b8c329fd4
lwz <905328134@qq.com>
2024-09-25 14:51:42 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_wiki.sh
+++ clip_openai/aaaa_train_bs1024_wiki.sh
@@ -129,7 +129,7 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   echo "Running training for dataset: ${DATASET_NAME} with lr: ${LR}"
 
   # 按不同的方法运行（比如 'distill_lora', 'vanilla'）
-#  run_training "vanilla" ${DATASET_NAME} ${LR} ${LR_TEXT}
+  run_training "vanilla" ${DATASET_NAME} ${LR} ${LR_TEXT}
 #  run_training "distill" ${DATASET_NAME} ${LR} ${LR_TEXT}
 #  run_training "lora_v2" ${DATASET_NAME} ${LR} ${LR_TEXT}
 #  run_training "lora" ${DATASET_NAME} ${LR} ${LR_TEXT}
@@ -140,5 +140,5 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   echo "Completed training for dataset: ${DATASET_NAME}"
 done
 
-#/ppio_net0/code/openapi.sh stop 46e358198c65fd38
+/ppio_net0/code/openapi.sh stop 4ba89aed10a162b3
 
