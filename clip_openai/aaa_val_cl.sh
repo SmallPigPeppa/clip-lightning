1d8ac1f5bc69a8b782390fc0cefcdc6f669dcf7e
lwz <905328134@qq.com>
2024-09-25 16:38:36 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_cl.sh
+++ clip_openai/aaa_val_cl.sh
@@ -13,7 +13,7 @@ DATASETS=("coco2014")
 CKPTS=(
   "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
   "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
-#  "ckpt/pet-1024/distill_lora_v2-lr-1e-5-lr_text-4e-4.ckpt"
+  "ckpt/pet-1024/distill_lora_v2-lr-1e-5-lr_text-4e-4.ckpt"
 )
 
 
