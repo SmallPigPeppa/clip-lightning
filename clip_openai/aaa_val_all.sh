5481538e32a7ecf7457d0b82706d43de91aa8e18
lwz <905328134@qq.com>
2024-09-26 04:16:32 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_all.sh
+++ clip_openai/aaa_val_all.sh
@@ -10,9 +10,9 @@ datasets=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "s
 datasets=(
 #"flickr30k"
 #"coco2014"
-#"pet"
+"pet"
 #"lexica"
-"simpsons"
+#"simpsons"
 #"patfig"
 #"wikiart"
 #"styles"
@@ -26,11 +26,14 @@ DATASETS=$(IFS=','; echo "${datasets[*]}")
 CKPTS=(
     "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
     "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
-    "ckpt/pet-1024/distill_lora_v2-lr-2e-5-lr_text-4e-5.ckpt"
-    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-3e-5.ckpt"
+#    "ckpt/pet-1024/distill_lora_v2-lr-2e-5-lr_text-4e-5.ckpt"
+    "ckpt/pet-1024/distill_lora_v2-lr-5e-4-lr_text-5e-4.ckpt"
+
+#    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-3e-5.ckpt"
 #    "ckpt/lexica-1024/distill_lora_v2-lr-3e-5-lr_text-3e-5-v1.ckpt"
 #    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-1.5e-4.ckpt"
-    "ckpt/simpsons-1024/distill_lora_v2-lr-4e-5-lr_text-4e-5.ckpt"
+
+#    "ckpt/simpsons-1024/distill_lora_v2-lr-4e-5-lr_text-4e-5.ckpt"
 )
 
 
