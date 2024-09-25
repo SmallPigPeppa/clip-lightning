137cec431de4d0b83eaaeed0c977e85ad2706ffd
lwz <905328134@qq.com>
2024-09-25 14:51:01 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_cl.sh
+++ clip_openai/aaa_val_cl.sh
@@ -76,7 +76,7 @@ for DATASET_NAME in "${DATASETS[@]}"; do
     --model.weight_decay 0.1 \
     --model.download_root ./ \
     --model.recall_eval_interval 1 \
-    --model.zero_shot_eval_interval 1 \
+    --model.zero_shot_eval_interval 2 \
     --model.old_checkpoint_path ${CKPTS_COMMA_JOINED} \
     --trainer.accelerator gpu \
     --trainer.precision 16 \
