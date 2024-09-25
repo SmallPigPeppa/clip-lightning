2dc3ca51d0756c73a6c7511c8b5718073ea9802f
lwz <905328134@qq.com>
2024-09-26 01:40:10 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_all.sh
+++ clip_openai/aaa_val_all.sh
@@ -27,7 +27,7 @@ CKPTS=(
     "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
     "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
     "ckpt/pet-1024/distill_lora_v2-lr-2e-5-lr_text-4e-5.ckpt"
-    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-3e-5.ckpt"
+#    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-3e-5.ckpt"
     "ckpt/lexica-1024/distill_lora_v2-lr-3e-5-lr_text-1.5e-4.ckpt"
 )
 
