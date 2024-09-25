23caac5343d9091ed4e027702e3b59208500f340
lwz <905328134@qq.com>
2024-09-25 14:03:59 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_pet.sh
+++ clip_openai/aaaa_train_bs1024_pet.sh
@@ -16,7 +16,7 @@ DATASETS=("pet")
 
 # 数据集学习率映射
 declare -A DATASET_LR_MAP=(
-  ["pet"]=1e-5
+  ["pet"]=5e-6
   ["flickr30k"]=8e-6
   ["coco2014"]=1e-5
   ["wikiart"]=1e-5
@@ -38,7 +38,7 @@ declare -A DATASET_LR_MAP=(
 
 
 declare -A DATASET_LR_TEXT_MAP=(
-  ["pet"]=3.5e-4
+  ["pet"]=1.75e-4
   ["flickr30k"]=1.2e-4
   ["coco2014"]=2e-4
   ["wikiart"]=2e-4
