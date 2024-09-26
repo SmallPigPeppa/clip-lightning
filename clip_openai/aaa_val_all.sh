08477beca7491a6be05db444d4e819d7d83d6c4e
lwz <905328134@qq.com>
2024-09-27 02:05:04 +0800

    initial

--- clip_openai/aaa_val_all.sh
+++ clip_openai/aaa_val_all.sh
@@ -56,11 +56,11 @@ DATASETS=$(IFS=','; echo "${datasets[*]}")
 #)
 
 CKPTS=(
-#    "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
-#    "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
-#    "ckpt/pet-1024/distill-lr-3e-5-lr_text-3e-5.ckpt"
-#    "ckpt/lexica-1024/distill-lr-1e-4-lr_text-1e-4.ckpt"
-#    "ckpt/simpsons-1024/distill-lr-1e-4-lr_text-1e-4.ckpt"
+    "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
+    "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
+    "ckpt/pet-1024/distill-lr-3e-5-lr_text-3e-5.ckpt"
+    "ckpt/lexica-1024/distill-lr-1e-4-lr_text-1e-4.ckpt"
+    "ckpt/simpsons-1024/distill-lr-5e-5-lr_text-5e-5.ckpt"
     "ckpt/patfig-1024/distill-lr-3e-5-lr_text-3e-5.ckpt"
 )
 
