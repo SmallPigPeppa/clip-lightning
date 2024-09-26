12f0b2bdf43d1091d3faa3a959ac637f18d127ae
lwz <905328134@qq.com>
2024-09-26 20:59:57 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_simpson.sh
+++ clip_openai/aaaa_train_bs1024_simpson.sh
@@ -164,6 +164,6 @@ for DATASET_NAME in "${DATASETS[@]}"; do
   echo "Completed training for dataset: ${DATASET_NAME}"
 done
 
-/ppio_net0/code/openapi.sh stop 4ba89aed10a162b3
+/ppio_net0/code/openapi.sh stop 14ee9a05e41fc7a4
 
 
