ea225ca9ba8c7bb4e0306dc84fec22c167b9f83d
lwz <905328134@qq.com>
2024-09-26 11:43:47 +0800

    initial

--- clip_openai/aaa_val_all.sh
+++ clip_openai/aaa_val_all.sh
@@ -27,22 +27,23 @@ CKPTS=(
     "ckpt/flickr30k-1024/distill_lora_v2-lr-8.6e-6-lr_text-1.3e-4.ckpt"
     "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
 
-    "ckpt/pet-1024/distill_lora_v2-lr-2e-5-lr_text-4e-5.ckpt"
+#    "ckpt/pet-1024/distill_lora_v2-lr-2e-5-lr_text-4e-5.ckpt"
 #   "ckpt/pet-1024/distill_lora_v2-lr-2e-4-lr_text-2e-4.ckpt"
+   "ckpt/pet-1024/distill_lora_v2-lr-1e-4-lr_text-1e-4.ckpt"
 
-    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-3e-5.ckpt"
+
+#    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-3e-5.ckpt"
 #    "ckpt/lexica-1024/distill_lora_v2-lr-3e-5-lr_text-3e-5-v1.ckpt"
 #    "ckpt/lexica-1024/lora_v2-lr-3e-5-lr_text-1.5e-4.ckpt"
 #    "ckpt/lexica-1024/lora_v2-lr-5e-4-lr_text-5e-4.ckpt"
 #    "ckpt/lexica-1024/lora_v2-lr-1.99e-4-lr_text-1.99e-4.ckpt"
 #    "ckpt/lexica-1024/lora_v2-lr-2e-4-lr_text-2e-4.ckpt"
-#    "ckpt/lexica-1024/lora_v2-lr-1e-4-lr_text-1e-4.ckpt"
 
 #    "ckpt/lexica-1024/lora_v2-lr-6e-5-lr_text-5e-4.ckpt"
 
 #    "ckpt/simpsons-1024/distill_lora_v2-lr-4e-5-lr_text-4e-5.ckpt"
 #    "ckpt/simpsons-1024/distill_lora_v2-lr-2e-4-lr_text-2e-4.ckpt"
-    "ckpt/simpsons-1024/distill_lora_v2-lr-5e-4-lr_text-5e-4.ckpt"
+#    "ckpt/simpsons-1024/distill_lora_v2-lr-5e-4-lr_text-5e-4.ckpt"
 )
 
 
