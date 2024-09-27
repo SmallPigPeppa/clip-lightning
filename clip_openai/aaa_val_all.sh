f0e356a466c11842a5e309d87f4100f96289d95b
lwz <905328134@qq.com>
2024-09-27 12:25:41 +0800

    initial

--- clip_openai/aaa_val_all.sh
+++ clip_openai/aaa_val_all.sh
@@ -60,8 +60,8 @@ CKPTS=(
     "ckpt/coco2014-1024/distill_lora_v2-lr-5e-7-lr_text-4e-5.ckpt"
     "ckpt/pet-1024/distill-lr-3e-5-lr_text-3e-5.ckpt"
     "ckpt/lexica-1024/distill-lr-1e-4-lr_text-1e-4.ckpt"
-#    "ckpt/simpsons-1024/distill-lr-5e-5-lr_text-5e-5.ckpt"
-    "ckpt/patfig-1024/distill-lr-5e-5-lr_text-5e-5.ckpt"
+    "ckpt/simpsons-1024/distill-lr-5e-5-lr_text-5e-5.ckpt"
+    "ckpt/patfig-1024/distill_lora_v2-lr-5e-4-lr_text-5e-4"
 )
 
 # 拼接检查点
