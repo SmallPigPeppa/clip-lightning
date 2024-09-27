9a3bd517f288b293221a9b49198a721549ed6ccb
lwz <905328134@qq.com>
2024-09-28 04:08:27 +0800

    update to lightning 2.0

--- clip_openai/aaaa_train_bs1024_other_methods_ft.sh
+++ clip_openai/aaaa_train_bs1024_other_methods_ft.sh
@@ -12,7 +12,7 @@ MODEL_NAME=ViT-B/16
 DATASETS=("flickr30k" "coco2014" "pet" "lexica" "simpsons" "wikiart" "kream" "sketch")
 #DATASETS=("coco2014" "pet" "lexica" "simpsons" "wikiart")
 
-LR=2e-5
+LR=3e-5
 
 # 脚本方法映射
 declare -A METHOD_MAP=(
