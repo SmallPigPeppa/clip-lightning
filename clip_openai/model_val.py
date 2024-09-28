bb160e132029a10319fe6c7930a2e988d8b58a7d
lwz <905328134@qq.com>
2024-09-28 21:26:18 +0800

    update to lightning 2.0

--- clip_openai/model_val.py
+++ clip_openai/model_val.py
@@ -166,7 +166,7 @@ class CLIPDualEncoderModel(LightningModule):
 
         # if self.global_rank == 0:
         dataset_all = self.trainer.datamodule.dataset_name  # assuming dataset names are in the datamodule
-        import pdb;pdb.set_trace()
+        # import pdb;pdb.set_trace()
         all_metrics = []
         self.model.eval()
 
@@ -176,8 +176,6 @@ class CLIPDualEncoderModel(LightningModule):
                 recall_metric = self.get_recall_metrics_5caption(val_loader, num_caption=5)
             else:
                 recall_metric = self.get_recall_metrics_1caption(val_loader)
-                import pdb;
-                pdb.set_trace()
                 if recall_metric is None:
                     import pdb;pdb.set_trace()
 
