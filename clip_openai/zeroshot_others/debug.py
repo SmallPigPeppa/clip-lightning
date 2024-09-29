9768e813934eaa537c388642060474ddbcd89afe
lwz <905328134@qq.com>
2024-09-29 21:45:00 +0800

    update to lightning 2.0

--- clip_openai/zeroshot_others/debug.py
+++ clip_openai/zeroshot_others/debug.py
@@ -4,4 +4,5 @@ data_path='/ppio_net0/torch_ds'
 dataset_test = datasets.STL10(root=data_path, split='test', download=True, transform=None)
 dataset_test = datasets.OxfordIIITPet(root=data_path, split='test', download=True, target_types='category',transform=None)
 dataset_test = datasets.StanfordCars(root=data_path, split='test', download=False, transform=None)
-# dataset_test = datasets.FGVCAircraft(root=data_path, split='test', download=True, transform=None)
+dataset_test = datasets.FGVCAircraft(root=data_path, split='test', download=True, transform=None)
+datasets.Flowers102(root=data_path, split='test', download=True, transform=None)
