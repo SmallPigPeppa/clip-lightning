bdb58c92b94414ea29dcbf467b1032fd7c2a135e
lwz <905328134@qq.com>
2024-09-30 02:25:26 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_zeroshot.sh
+++ clip_openai/aaa_val_zeroshot.sh
@@ -9,14 +9,14 @@ MODEL_NAME=ViT-B/16
 # 数据集列表
 datasets=(
 #  "cifar100"
-   "stl10"
+#   "stl10"
    "flowers"
    "pets"
    "cars"
    "food101"
 #   "dtd"
    "aircraft"
-   "cifar10"
+#   "cifar10"
 )
 
 # 将 datasets 数组转换为逗号分隔的字符串
--- clip_openai/zeroshot_others/dataset_others.py
+++ clip_openai/zeroshot_others/dataset_others.py
@@ -283,17 +283,17 @@ def get_food101(data_path):
             transforms.Resize((256, 256)),
             transforms.CenterCrop(size=224),
             transforms.ToTensor(),
-            # transforms.Normalize((0.4832, 0.4449, 0.3951), (0.2160, 0.2131, 0.2136)),
+            transforms.Normalize((0.5545, 0.4432, 0.3324), (0.2222, 0.2340, 0.2303)),
         ]
     )
-    # dataset_train = datasets.Food101(root=data_path, download=True, transform=transform_train)
+    dataset_train = datasets.Food101(root=data_path, download=True, transform=transform_train)
     dataset_test = datasets.Food101(root=data_path, download=True, transform=transform_test)
     # mean_train, std_train = get_statistics(dataset_train)
-    mean_test, std_test = get_statistics(dataset_test)
+    # mean_test, std_test = get_statistics(dataset_test)
 
     # print(f'Train dataset: mean={mean_train}, std={std_train}')
-    print(f'Test dataset: mean={mean_test}, std={std_test}')
-    # return dataset_train, dataset_test
+    # print(f'Test dataset: mean={mean_test}, std={std_test}')
+    return dataset_train, dataset_test
 
 
 def get_dataset(data_path, dataset_name):
