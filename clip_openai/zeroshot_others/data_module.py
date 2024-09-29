3d7b0036ba964996aed60685e0ea010a1396dc66
lwz <905328134@qq.com>
2024-09-29 20:47:01 +0800

    update to lightning 2.0

--- clip_openai/zeroshot_others/data_module.py
+++ clip_openai/zeroshot_others/data_module.py
@@ -42,7 +42,15 @@ class ZeroshotDataModule(LightningDataModule):
             self.setup_single_dataset(dataset_name, stage)
 
     def zero_shot_dataloader(self, dataset_name=None):
-        # dataset_name = dataset_name or self.dataset_name[0]  # Default to the first dataset if none is provided
+        return DataLoader(
+            self.datasets[dataset_name]["val"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self, dataset_name=None):
+        dataset_name = dataset_name or self.dataset_name[0]  # Default to the first dataset if none is provided
         return DataLoader(
             self.datasets[dataset_name]["val"],
             batch_size=self.batch_size,
