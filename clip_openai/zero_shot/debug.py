a94737fef824b633b2fd739359bc7e65bd37baa6
lwz <905328134@qq.com>
2024-09-29 19:27:55 +0800

    update to lightning 2.0

--- clip_openai/zero_shot/debug.py
+++ clip_openai/zero_shot/debug.py
@@ -2,4 +2,6 @@ from torchvision import datasets
 data_path='/ppio_net0/torch_ds'
 # dataset_train = datasets.SUN397(root=data_path, download=True, transform=None)
 dataset_test = datasets.STL10(root=data_path, split='test', download=True, transform=None)
-
+dataset_test = datasets.OxfordIIITPet(root=data_path, split='test', download=True, target_types='category',transform=None)
+dataset_test = datasets.StanfordCars(root=data_path, split='test', download=False, transform=None)
+dataset_test = datasets.FGVCAircraft(root=data_path, split='test', download=True, transform=None)
