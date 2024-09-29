fbefbdc95c5cb85d64d162c9f6b6fb411713c709
lwz <905328134@qq.com>
2024-09-29 19:02:11 +0800

    update to lightning 2.0

--- clip_openai/zero_shot/debug.py
+++ clip_openai/zero_shot/debug.py
@@ -1,3 +1,4 @@
 from torchvision import datasets, transforms
 data_path='/ppio_net0/torch_ds'
 datasets.Caltech101(root=data_path, target_type='category', download=True, transform=None)
+dataset_train = datasets.Flowers102(root=data_path, split='train', download=True, transform=None)
