e5e8acef23a91e5258bdfc30c37fa9f9e4a8190d
lwz <905328134@qq.com>
2024-11-23 01:15:40 +0800

    update to lightning 2.0

--- clip_openai/aaa_val_clip.sh
+++ clip_openai/aaa_val_clip.sh
@@ -9,7 +9,7 @@ MODEL_NAME=ViT-B/16
 DATASETS=("flickr30k" "coco2014" "wikiart" "patfig" "pet" "simpsons" "lexica" "styles" "kream" "sketch")
 DATASETS=("flickr30k")
 #DATASETS=("wikiart")
-DATASETS=("coco2014")
+#DATASETS=("coco2014")
 
 
 
