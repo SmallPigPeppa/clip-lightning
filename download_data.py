9661c6d6997d717bb2acd9b2fef68fe929d51942
lwz <905328134@qq.com>
2025-06-16 22:51:32 +0800

    update to lightning 2.0

--- download_data.py
+++ download_data.py
@@ -3,5 +3,9 @@ import kagglehub
 os.environ["KAGGLEHUB_CACHE"] = "/mnt/hdfs/byte_content_security/user/liuwenzhuo/kagglehub_cache"
 # path = kagglehub.dataset_download("hsankesara/flickr-image-dataset")
 # print("Path to dataset files:", path)
-path = kagglehub.dataset_download("nikhil7280/coco-image-caption")
+# path = kagglehub.dataset_download("nikhil7280/coco-image-caption")
+# print("Path to dataset files:", path)
+
+
+path = kagglehub.dataset_download("lyte69/gqa-images")
 print("Path to dataset files:", path)
