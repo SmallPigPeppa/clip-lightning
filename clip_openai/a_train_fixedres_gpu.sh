b6a3ba84f7eaf29a8c3be9b5caa7ec8f86775990
lwz <905328134@qq.com>
2025-06-18 01:12:18 +0800

    update to lightning 2.0

--- clip_openai/a_train_fixedres_gpu.sh
+++ clip_openai/a_train_fixedres_gpu.sh
@@ -2,11 +2,11 @@
 
 
 # 设置 Hugging Face home 目录
-export HF_HOME=/mnt/bn/liuwenzhuo-hl-data/hf_cache
+export HF_HOME=/mnt/bn/liuwenzhuo-hl-data/ckpt/hf_cache
 export WANDB_BASE_URL=https://api.bandw.top
-ROOT_DIR=/mnt/bn/liuwenzhuo-hl-data/datasets
-MODEL_DIR=/mnt/bn/liuwenzhuo-hl-data/openclip_cache
-CKPT_DIR=/mnt/bn/liuwenzhuo-hl-data/ckpt/clip_msun
+ROOT_DIR=/mnt/bn/liuwenzhuo-hl-data/ckpt/datasets
+MODEL_DIR=/mnt/bn/liuwenzhuo-hl-data/ckpt/openclip_cache
+CKPT_DIR=/mnt/bn/liuwenzhuo-hl-data/ckpt/ckpt/clip_msun
 PROJECT=CLIP-MSUN
 
 # 模型名称
--- download_data.py
+++ download_data.py
@@ -1,6 +1,6 @@
 import os
 import kagglehub
-os.environ["KAGGLEHUB_CACHE"] = "/mnt/bn/liuwenzhuo-hl-data/kagglehub_cache"
+os.environ["KAGGLEHUB_CACHE"] = "/mnt/bn/liuwenzhuo-hl-data/ckpt/ckpt/kagglehub_cache"
 # path = kagglehub.dataset_download("hsankesara/flickr-image-dataset")
 # print("Path to dataset files:", path)
 # path = kagglehub.dataset_download("nikhil7280/coco-image-caption")
