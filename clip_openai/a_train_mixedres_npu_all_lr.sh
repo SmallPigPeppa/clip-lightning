2ac06057f48ff7890509ce33b175bca24ba9f403
lwz <905328134@qq.com>
2025-06-20 16:00:17 +0800

    update to lightning 2.0

--- clip_openai/a_train_mixedres_npu_all_lr.sh
+++ clip_openai/a_train_mixedres_npu_all_lr.sh
@@ -8,7 +8,7 @@ export WANDB_BASE_URL=https://api.bandw.top
 
 ROOT_DIR=/mnt/hdfs/byte_content_security/user/liuwenzhuo/datasets
 MODEL_DIR=/mnt/bn/liuwenzhuo-hl-data/openclip_cache
-CKPT_DIR=/mnt/bn/liuwenzhuo-hl-data/ckpt/clip_msun/fixedres
+CKPT_DIR=/mnt/bn/liuwenzhuo-hl-data/ckpt/clip_msun/mixedres
 PROJECT=CLIP-MSUN
 MODEL_NAME=RN101
 
