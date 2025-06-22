ce72478e11aae5f25095ca0b04231b03a4a8663a
lwz <905328134@qq.com>
2025-06-22 17:17:26 +0800

    update to lightning 2.0

--- clip_openai/a_train_mixedres_npu_all_lr.sh
+++ clip_openai/a_train_mixedres_npu_all_lr.sh
@@ -69,7 +69,7 @@ run_training() {
     --trainer.log_every_n_steps 1 \
     --trainer.logger WandbLogger \
     --trainer.logger.project ${PROJECT} \
-    --trainer.logger.name mixedres-{dataset}-${MODEL_NAME}-lr${lr}-bs${TOTAL_BATCH_SIZE} \
+    --trainer.logger.name mixedres-${dataset}-${MODEL_NAME}-lr${lr}-bs${TOTAL_BATCH_SIZE} \
     --trainer.logger.log_model False \
     --trainer.logger.offline False \
     --trainer.strategy ddp_find_unused_parameters_true \
