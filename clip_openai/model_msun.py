7e38110f1f4530282cfcc85b63fd9741ac7c158f
lwz <905328134@qq.com>
2025-06-23 00:09:11 +0800

    update to lightning 2.0

--- clip_openai/model_msun.py
+++ clip_openai/model_msun.py
@@ -92,6 +92,7 @@ class CLIPDualEncoderModel(LightningModule):
 
         # make subnet1/2 conv1 stride =1
         visual.subnet1[0].stride = (1, 1)
+        visual.subnet1[9]=nn.Identity()
         visual.subnet2[0].stride = (1, 1)
 
         # resolution configs
