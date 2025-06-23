021ebf8a490581cc83076be4913b7aa19e9913f1
lwz <905328134@qq.com>
2025-06-23 15:11:31 +0800

    update to lightning 2.0

---
+++ clip_openai/cli_val_anyres_msun_block4.py
@@ -0,0 +1,24 @@
+from lightning.pytorch import cli
+from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from lightning.pytorch.callbacks.lr_monitor import LearningRateMonitor
+from dataloaders.data_module_dil_json_zeroshot_hf_all import ImageRetrievalDataModule
+from model_val_anyres_msun_block4 import CLIPDualEncoderModel
+
+
+class CLI(cli.LightningCLI):
+    def add_arguments_to_parser(self, parser: cli.LightningArgumentParser) -> None:
+        parser.link_arguments(
+            "data.batch_size", "model.batch_size"
+        )
+        parser.link_arguments(
+            "data.batch_size_zs", "model.batch_size_zs"
+        )
+        parser.link_arguments(
+            "data.current_task", "model.current_task"
+        )
+        parser.add_lightning_class_args(ModelCheckpoint, "model_checkpoint")
+        parser.add_lightning_class_args(LearningRateMonitor, "lr_monitor")
+
+
+if __name__ == "__main__":
+    CLI(CLIPDualEncoderModel, ImageRetrievalDataModule, save_config_callback=None, seed_everything_default=6)
